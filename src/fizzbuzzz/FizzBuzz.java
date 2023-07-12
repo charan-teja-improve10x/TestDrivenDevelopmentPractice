3b801914d9526a1d1ffe7d1c1d6900afe512f08e
CHARAN TEJA <charan.teja@improve10x.com>
2023-07-12 16:06:30 +0530

    createdFizzBuzz class and testcase is passed

---
+++ src/fizzbuzzz/FizzBuzz.java
@@ -0,0 +1,31 @@
+package fizzbuzzz;
+
+import scoringsystem.Score;
+
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+        String print = findFizzBuzz(7);
+        System.out.println(print);
+    }
+
+    public static String findFizzBuzz(int n) {
+        String result = "";
+        if (n < 0) {
+            result = "-1";
+        } else if (n == 1) {
+            result = 1 + "";
+        } else if (n == 2) {
+            result = 2 + "";
+        } else if (n % 3 == 0 && n % 5 == 0) {
+            result = "FizzBuzz";
+        } else if (n % 3 == 0) {
+            result = "Fizz";
+        } else if (n % 5 == 0) {
+            result = "Buzz";
+        } else {
+            result = n + "";
+        }
+        return result;
+    }
+}
---
+++ src/fizzbuzzz/FizzBuzzTest.java
@@ -0,0 +1,62 @@
+package fizzbuzzz;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FizzBuzzTest {
+
+    private FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    public void setup() {
+        fizzBuzz = new FizzBuzz();
+    }
+
+    @Test
+    public void nothing() {}
+
+    @Test
+    public void givenNegative_returnNegative() {
+        String negative = fizzBuzz.findFizzBuzz(-10);
+        assertEquals("-1", negative);
+    }
+
+    @Test
+    public void givenZero_returnZero() {
+        String negative = fizzBuzz.findFizzBuzz(0);
+        assertEquals(negative, negative);
+    }
+
+    @Test
+    public void givenOne_returnOne() {
+        String givenOne = fizzBuzz.findFizzBuzz(1);
+        assertEquals(givenOne, givenOne);
+    }
+
+    @Test
+    public void givenTwo_returnTwo() {
+        String givenTwo = fizzBuzz.findFizzBuzz(2);
+        assertEquals(givenTwo, givenTwo);
+    }
+
+    @Test
+    public void givenThree_returnFizz() {
+        String givenThree = fizzBuzz.findFizzBuzz(3);
+        assertEquals("Fizz", givenThree);
+    }
+
+    @Test
+    public void givenFive_returnBuzz() {
+        String givenFive = fizzBuzz.findFizzBuzz(5);
+        assertEquals("Buzz", givenFive);
+    }
+
+    @Test
+    public void givenFifteen_returnFizzBuzz() {
+        String givenFifteen = fizzBuzz.findFizzBuzz(15);
+        assertEquals("FizzBuzz", givenFifteen);
+    }
+}
