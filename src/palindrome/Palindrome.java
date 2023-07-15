991b753cb27093a869e5b8c4305d7746af1433f9
CHARAN TEJA <charan.teja@improve10x.com>
2023-07-15 23:03:36 +0530

    created PalindromeTest class to test the word is palindrome or not

---
+++ src/palindrome/Palindrome.java
@@ -0,0 +1,22 @@
+package palindrome;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("121"));
+    }
+    public static boolean isPalindrome(String name) {
+        String reverse = "";
+        String wordTrim = name.trim();
+        for (int i = wordTrim.length() -1; i >= 0; i--) {
+            reverse += wordTrim.charAt(i);
+        }
+
+        boolean result = false;
+        if (wordTrim != null && wordTrim.equals(reverse)) {
+            result = true;
+        } else {
+            result = false;
+        }
+        return result;
+    }
+}
