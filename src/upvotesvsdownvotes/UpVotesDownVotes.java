333810b5d7f0d252ebc9488e7184ef755c921871
CHARAN TEJA <charan.teja@improve10x.com>
2023-07-15 17:26:11 +0530

    created UpVotesDownVotesTest to calculate the difference between upVotes and downVotes

---
+++ src/upvotesvsdownvotes/UpVotesDownVotes.java
@@ -0,0 +1,13 @@
+package upvotesvsdownvotes;
+
+public class UpVotesDownVotes {
+
+    public static void main(String[] args) {
+        System.out.println(getVoteCount(-33, -22));
+    }
+    public static int getVoteCount(int upVotes, int downVotes) {
+        int result = 0;
+        result = upVotes - downVotes;
+        return result;
+    }
+}
---
+++ src/upvotesvsdownvotes/UpVotesDownVotesTest.java
@@ -0,0 +1,55 @@
+package upvotesvsdownvotes;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UpVotesDownVotesTest {
+
+    private UpVotesDownVotes upVotesDownVotes;
+
+    @BeforeEach
+    public void setup(){
+        upVotesDownVotes = new UpVotesDownVotes();
+    }
+
+    @Test
+    public void nothing(){}
+
+    @Test
+    public void givenTwoZeros_returnZero(){
+        int returnZero = upVotesDownVotes.getVoteCount(0, 0);
+        assertEquals(0, returnZero);
+    }
+
+    @Test
+    public void givenOneZero_returnOne(){
+        int returnOne = upVotesDownVotes.getVoteCount(1, 0);
+        assertEquals(1, returnOne);
+    }
+
+    @Test
+    public void givenTwoSameNegativeNumbers_returnZero(){
+        int returnOne = upVotesDownVotes.getVoteCount(1, 0);
+        assertEquals(1, returnOne);
+    }
+
+    @Test
+    public void givenMinus33AndMinus22_returnMinus11(){
+        int returnMinus11 = upVotesDownVotes.getVoteCount(-33, -22);
+        assertEquals(-11, returnMinus11);
+    }
+
+    @Test
+    public void given56And44_return100(){
+        int return12 = upVotesDownVotes.getVoteCount(56, 44);
+        assertEquals(12, return12);
+    }
+
+    @Test
+    public void givenTwoSameNumbers_return0(){
+        int return0 = upVotesDownVotes.getVoteCount(56, 56);
+        assertEquals(0, return0);
+    }
+}
