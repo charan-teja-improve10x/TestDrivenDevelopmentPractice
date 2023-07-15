e9c3e2821514bdfea4c8f9f9a8545766bdba31c0
CHARAN TEJA <charan.teja@improve10x.com>
2023-07-15 13:05:28 +0530

    created IntegerInRangeTest and wrote test cases

---
+++ src/integerinrange/IntegerInRange.java
@@ -0,0 +1,13 @@
+package integerinrange;
+
+public class IntegerInRange {
+    public boolean findIntegerRange(int num, int lowerBounds, int upperBounds) {
+        boolean result = false;
+        if (num == 0 && lowerBounds == 0 && upperBounds == 0){
+            result = false;
+        }else if (num >= lowerBounds && num < upperBounds){
+            result = true;
+        }
+        return result;
+    }
+}
---
+++ src/integerinrange/IntegerInRangeTest.java
@@ -0,0 +1,48 @@
+package integerinrange;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class IntegerInRangeTest {
+
+    private IntegerInRange integerInRange;
+
+    @BeforeEach
+    public void setup(){
+        integerInRange = new IntegerInRange();
+    }
+    @Test
+    public void nothing(){}
+
+    @Test
+    public void givenAllZeros_returnFalse(){
+        boolean returnFalse = integerInRange.findIntegerRange(0, 0, 0);
+        assertEquals(false, returnFalse);
+    }
+
+    @Test
+    public void givenThreeZeroFour_returnTrue(){
+        boolean returnTrue = integerInRange.findIntegerRange(3, 0, 4);
+        assertEquals(true, returnTrue);
+    }
+
+    @Test
+    public void givenThreeZeroZero_returnFalse(){
+        boolean returnFalse = integerInRange.findIntegerRange(3, 0, 0);
+        assertEquals(false, returnFalse);
+    }
+
+    @Test
+    public void givenThreeZeroThree_returnFalse(){
+        boolean returnFalse = integerInRange.findIntegerRange(3, 0, 0);
+        assertEquals(false, returnFalse);
+    }
+
+    @Test
+    public void givenThreeZeroZero_returnTrue(){
+        boolean returnFalse = integerInRange.findIntegerRange(3, 10, 10);
+        assertEquals(false, returnFalse);
+    }
+}
