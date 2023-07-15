08994123736f5a777b4369beb00ff3d3120fc08b
CHARAN TEJA <charan.teja@improve10x.com>
2023-07-16 00:09:54 +0530

    created MultiplyByLength class and i got struck in test cases

---
+++ src/multiplybylength/MultiplyByLength.java
@@ -0,0 +1,14 @@
+package multiplybylength;
+
+import java.util.Arrays;
+
+public class MultiplyByLength {
+
+    public static void main(String[] args) {
+
+    }
+    public static int[] multipleTheElementsWithLength(int[] numbers) {
+
+        return new int[]{0};
+    }
+}
--- src/multiplybylength/MultiplyByLengthTest.java
+++ src/multiplybylength/MultiplyByLengthTest.java
@@ -2,8 +2,19 @@ package multiplybylength;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MultiplyByLengthTest {
 
     @Test
     public void nothing(){}
+
+    @Test
+    public void givenZero_returnZero(){
+        MultiplyByLength multiplyByLength = new MultiplyByLength();
+        int[] returnArray = multiplyByLength.multipleTheElementsWithLength(new int[] {0});
+        assertEquals(new int[]{0}, returnArray);
+    }
 }
