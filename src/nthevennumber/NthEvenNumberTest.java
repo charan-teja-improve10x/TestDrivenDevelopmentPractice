fb21cd4f2b3657f400d4b74154da1ec2982d0be7
CHARAN TEJA <charan.teja@improve10x.com>
2023-07-16 23:52:59 +0530

    some code refactoring in NthEvenNumberTest class

--- src/nthevennumber/NthEvenNumberTest.java
+++ src/nthevennumber/NthEvenNumberTest.java
@@ -1,52 +1,54 @@
 package nthevennumber;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NthEvenNumberTest {
 
+    private NthEvenNumber nthEvenNumber;
+
+    @BeforeEach
+    public void setup(){
+        nthEvenNumber = new NthEvenNumber();
+    }
+
     @Test
     public void nothing(){}
 
     @Test
     public void givenZero_returnZero(){
-        NthEvenNumber nthEvenNumber = new NthEvenNumber();
         int evenNumber = nthEvenNumber.findTheNthEvenNumber(0);
         assertEquals(0, evenNumber);
     }
 
     @Test
     public void givenOne_returnZero(){
-        NthEvenNumber nthEvenNumber = new NthEvenNumber();
         int evenNumber = nthEvenNumber.findTheNthEvenNumber(1);
         assertEquals(0, evenNumber);
     }
 
     @Test
     public void given2_return2(){
-        NthEvenNumber nthEvenNumber = new NthEvenNumber();
         int evenNumber = nthEvenNumber.findTheNthEvenNumber(2);
         assertEquals(2, evenNumber);
     }
 
     @Test
     public void given3_return4(){
-        NthEvenNumber nthEvenNumber = new NthEvenNumber();
         int evenNumber = nthEvenNumber.findTheNthEvenNumber(3);
         assertEquals(4, evenNumber);
     }
 
     @Test
     public void given10_return18(){
-        NthEvenNumber nthEvenNumber = new NthEvenNumber();
         int evenNumber = nthEvenNumber.findTheNthEvenNumber(10);
         assertEquals(18, evenNumber);
     }
 
     @Test
     public void given99_return196(){
-        NthEvenNumber nthEvenNumber = new NthEvenNumber();
         int evenNumber = nthEvenNumber.findTheNthEvenNumber(99);
         assertEquals(196, evenNumber);
     }
