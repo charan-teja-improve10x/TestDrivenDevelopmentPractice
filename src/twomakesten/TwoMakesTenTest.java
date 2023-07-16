38b21421734edeca245746c8c371e5228c354503
CHARAN TEJA <charan.teja@improve10x.com>
2023-07-16 23:11:25 +0530

    some code improvements done in TwoMakesTenTest.java class

--- src/twomakesten/TwoMakesTenTest.java
+++ src/twomakesten/TwoMakesTenTest.java
@@ -1,45 +1,48 @@
 package twomakesten;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TwoMakesTenTest {
 
+    private TwoMakesTen twoMakesTen;
+
+    @BeforeEach
+    public void setup(){
+        twoMakesTen = new TwoMakesTen();
+    }
+
     @Test
     public void nothing(){}
 
     @Test
     public void givenNegativeNumbers (){
-        TwoMakesTen twoMakesTen = new TwoMakesTen();
         boolean num = twoMakesTen.getTwoMakesTen(-1, -2);
         assertEquals(false, num);
     }
 
     @Test
     public void givenZero_returnFalse (){
-        TwoMakesTen twoMakesTen = new TwoMakesTen();
         boolean num = twoMakesTen.getTwoMakesTen(0, 0);
         assertEquals(false, num);
     }
 
     @Test
     public void givenTwoAndNine_returnFalse (){
-        TwoMakesTen twoMakesTen = new TwoMakesTen();
         boolean num = twoMakesTen.getTwoMakesTen(2, 9);
         assertEquals(false, num);
     }
 
     @Test
     public void givenTenOne_returnTrue (){
-        TwoMakesTen twoMakesTen = new TwoMakesTen();
         boolean num = twoMakesTen.getTwoMakesTen(10, 1);
         assertEquals(true, num);
     }
 
     @Test
     public void givenIPlusNTen_returnTrue (){
-        TwoMakesTen twoMakesTen = new TwoMakesTen();
         boolean num = twoMakesTen.getTwoMakesTen(4, 6);
         assertEquals(true, num);
     }
