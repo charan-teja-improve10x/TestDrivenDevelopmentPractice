32519b42364f20e55f59611885fa9d37900dbc55
CHARAN TEJA <charan.teja@improve10x.com>
2023-07-16 23:07:37 +0530

    some code improvements done in PerimeterOfRectangleTest class

--- src/perimeterofrectangle/PerimeterOfRectangleTest.java
+++ src/perimeterofrectangle/PerimeterOfRectangleTest.java
@@ -1,39 +1,44 @@
 package perimeterofrectangle;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PerimeterOfRectangleTest {
 
-    @Test
-    public void nothing(){}
+    private PerimeterOfRectangle perimeter;
+
+    @BeforeEach
+    public void setup() {
+        perimeter = new PerimeterOfRectangle();
+    }
 
     @Test
-    public void givenAllZeros_returnZero(){
-        PerimeterOfRectangle perimeter = new PerimeterOfRectangle();
-        int perimeterOfRectangle  = perimeter.findThePerimeterOfRectangle(0, 0);
+    public void nothing() {
+    }
+
+    @Test
+    public void givenAllZeros_returnZero() {
+        int perimeterOfRectangle = perimeter.findThePerimeterOfRectangle(0, 0);
         assertEquals(0, perimeterOfRectangle);
     }
 
     @Test
-    public void givenZeroAnd5_return10(){
-        PerimeterOfRectangle perimeter = new PerimeterOfRectangle();
-        int perimeterOfRectangle  = perimeter.findThePerimeterOfRectangle(0, 5);
+    public void givenZeroAnd5_return10() {
+        int perimeterOfRectangle = perimeter.findThePerimeterOfRectangle(0, 5);
         assertEquals(10, perimeterOfRectangle);
     }
 
     @Test
-    public void givenMinusValue_returnMinusOne(){
-        PerimeterOfRectangle perimeter = new PerimeterOfRectangle();
-        int perimeterOfRectangle  = perimeter.findThePerimeterOfRectangle(1, -5);
+    public void givenMinusValue_returnMinusOne() {
+        int perimeterOfRectangle = perimeter.findThePerimeterOfRectangle(1, -5);
         assertEquals(-1, perimeterOfRectangle);
     }
 
     @Test
-    public void given2And5_return14(){
-        PerimeterOfRectangle perimeter = new PerimeterOfRectangle();
-        int perimeterOfRectangle  = perimeter.findThePerimeterOfRectangle(2, 5);
+    public void given2And5_return14() {
+        int perimeterOfRectangle = perimeter.findThePerimeterOfRectangle(2, 5);
         assertEquals(14, perimeterOfRectangle);
     }
 }
