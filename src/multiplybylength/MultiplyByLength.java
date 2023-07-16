00689738bcac356d99b84aae7a670add607196e9
CHARAN TEJA <charan.teja@improve10x.com>
2023-07-16 19:11:57 +0530

    test cases done with MultiplyByLength.java

--- src/multiplybylength/MultiplyByLength.java
+++ src/multiplybylength/MultiplyByLength.java
@@ -5,10 +5,12 @@ import java.util.Arrays;
 public class MultiplyByLength {
 
     public static void main(String[] args) {
-
+        System.out.println(Arrays.toString(multipleTheElementsWithLength(new int[] {0, 0, 0})));
     }
     public static int[] multipleTheElementsWithLength(int[] numbers) {
-
-        return new int[]{0};
+        for (int i = 0; i < numbers.length; i++){
+            numbers[i] *= numbers.length;
+        }
+        return numbers;
     }
 }
--- src/multiplybylength/MultiplyByLengthTest.java
+++ src/multiplybylength/MultiplyByLengthTest.java
@@ -2,9 +2,8 @@ package multiplybylength;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MultiplyByLengthTest {
 
@@ -15,6 +14,41 @@ public class MultiplyByLengthTest {
     public void givenZero_returnZero(){
         MultiplyByLength multiplyByLength = new MultiplyByLength();
         int[] returnArray = multiplyByLength.multipleTheElementsWithLength(new int[] {0});
-        assertEquals(new int[]{0}, returnArray);
+        assertArrayEquals(new int[] {0}, returnArray);
+    }
+
+    @Test
+    public void givenOneTwoThree_return369(){
+        MultiplyByLength multiplyByLength = new MultiplyByLength();
+        int[] returnArray = multiplyByLength.multipleTheElementsWithLength(new int[] {1, 2, 3});
+        assertArrayEquals(new int[] {3, 6, 9}, returnArray);
+    }
+
+    @Test
+    public void given3912_return369(){
+        MultiplyByLength multiplyByLength = new MultiplyByLength();
+        int[] returnArray = multiplyByLength.multipleTheElementsWithLength(new int[] {3, 9, 12});
+        assertArrayEquals(new int[] {9, 27, 36}, returnArray);
+    }
+
+    @Test
+    public void given391223_return369(){
+        MultiplyByLength multiplyByLength = new MultiplyByLength();
+        int[] returnArray = multiplyByLength.multipleTheElementsWithLength(new int[] {3, 9, 12, 23});
+        assertArrayEquals(new int[] {12, 36, 48, 92}, returnArray);
+    }
+
+    @Test
+    public void givenMinusOneTwoMinus11And23_return369(){
+        MultiplyByLength multiplyByLength = new MultiplyByLength();
+        int[] returnArray = multiplyByLength.multipleTheElementsWithLength(new int[] {-1, 2, -11, 23});
+        assertArrayEquals(new int[] {-4, 8, -44, 92}, returnArray);
+    }
+
+    @Test
+    public void givenAllZeros_returnAll(){
+        MultiplyByLength multiplyByLength = new MultiplyByLength();
+        int[] returnArray = multiplyByLength.multipleTheElementsWithLength(new int[] {0, 0, 0, 0});
+        assertArrayEquals(new int[] {0, 0, 0, 0}, returnArray);
     }
 }
