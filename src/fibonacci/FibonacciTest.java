95282de753f2664fa4fc965a0f7070e9ae67227e
CHARAN TEJA <charan.teja@improve10x.com>
2023-07-16 22:59:40 +0530

    some code improvements done in FibonacciTest class

--- src/fibonacci/FibonacciTest.java
+++ src/fibonacci/FibonacciTest.java
@@ -1,10 +1,17 @@
 package fibonacci;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class FibonacciTest {
 
+    private Fibonacci fibonacci;
+
+    @BeforeEach
+    public void setup(){
+        fibonacci = new Fibonacci();
+    }
     @Test
     public void nothing(){
 
@@ -19,42 +26,36 @@ public class FibonacciTest {
 
     @Test
     public void givenZero_returnNegative() {
-        Fibonacci fibonacci = new Fibonacci();
         int zero = fibonacci.find(0);
         assertEquals(-1, zero);
     }
 
     @Test
     public void givenOne_returnZero() {
-        Fibonacci fibonacci = new Fibonacci();
         int one = fibonacci.find(1);
         assertEquals(0, one);
     }
 
     @Test
     public void givenTwo_returnOne() {
-        Fibonacci fibonacci = new Fibonacci();
         int givenTwo = fibonacci.find(2);
         assertEquals(1, givenTwo);
     }
 
     @Test
     public void givenThree_returnTwo() {
-        Fibonacci fibonacci = new Fibonacci();
         int givenThree = fibonacci.find(3);
         assertEquals(2, givenThree);
     }
 
     @Test
     public void givenFive_returnFive() {
-        Fibonacci fibonacci = new Fibonacci();
         int givenFive = fibonacci.find(5);
         assertEquals(5, givenFive);
     }
 
     @Test
     public void given6_returnEight() {
-        Fibonacci fibonacci = new Fibonacci();
         int givenEight = fibonacci.find(6);
         assertEquals(8, givenEight);
     }
