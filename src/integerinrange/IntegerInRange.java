426531a0672aa8fcafe8a08c6dbcade9e2ea09ff
CHARAN TEJA <charan.teja@improve10x.com>
2023-07-17 17:34:36 +0530

    I did some code refactoring in IntegerInRangeTest  class

--- src/integerinrange/IntegerInRange.java
+++ src/integerinrange/IntegerInRange.java
@@ -3,9 +3,9 @@ package integerinrange;
 public class IntegerInRange {
     public boolean findIntegerRange(int num, int lowerBounds, int upperBounds) {
         boolean result = false;
-        if (num == 0 && lowerBounds == 0 && upperBounds == 0){
+        if (num == 0 && lowerBounds == 0 && upperBounds == 0) {
             result = false;
-        }else if (num >= lowerBounds && num < upperBounds){
+        } else if (num >= lowerBounds && num < upperBounds) {
             result = true;
         }
         return result;
--- src/integerinrange/IntegerInRangeTest.java
+++ src/integerinrange/IntegerInRangeTest.java
@@ -10,38 +10,46 @@ public class IntegerInRangeTest {
     private IntegerInRange integerInRange;
 
     @BeforeEach
-    public void setup(){
+    public void setup() {
         integerInRange = new IntegerInRange();
     }
-    @Test
-    public void nothing(){}
 
     @Test
-    public void givenAllZeros_returnFalse(){
+    public void nothing() {
+    }
+
+    @Test
+    public void givenAllZeros_returnFalse() {
         boolean returnFalse = integerInRange.findIntegerRange(0, 0, 0);
         assertEquals(false, returnFalse);
     }
 
     @Test
-    public void givenThreeZeroFour_returnTrue(){
+    public void givenNegative_returnFalse() {
+        boolean returnTrue = integerInRange.findIntegerRange(0, -1, 1);
+        assertEquals(true, returnTrue);
+    }
+
+    @Test
+    public void givenThreeZeroFour_returnTrue() {
         boolean returnTrue = integerInRange.findIntegerRange(3, 0, 4);
         assertEquals(true, returnTrue);
     }
 
     @Test
-    public void givenThreeZeroZero_returnFalse(){
+    public void givenThreeZeroZero_returnFalse() {
         boolean returnFalse = integerInRange.findIntegerRange(3, 0, 0);
         assertEquals(false, returnFalse);
     }
 
     @Test
-    public void givenThreeZeroThree_returnFalse(){
-        boolean returnFalse = integerInRange.findIntegerRange(3, 0, 0);
+    public void givenThreeZeroThree_returnFalse() {
+        boolean returnFalse = integerInRange.findIntegerRange(3, 0, 3);
         assertEquals(false, returnFalse);
     }
 
     @Test
-    public void givenThreeZeroZero_returnTrue(){
+    public void givenThreeZeroZero_returnTrue() {
         boolean returnFalse = integerInRange.findIntegerRange(3, 10, 10);
         assertEquals(false, returnFalse);
     }
