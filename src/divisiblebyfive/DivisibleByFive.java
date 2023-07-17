0319e7c842a1039e03dc7134ff0899d6bb9daf12
CHARAN TEJA <charan.teja@improve10x.com>
2023-07-17 15:17:33 +0530

    done some code refactoring in DivisibleByFiveTest class

--- src/divisiblebyfive/DivisibleByFive.java
+++ src/divisiblebyfive/DivisibleByFive.java
@@ -2,10 +2,7 @@ package divisiblebyfive;
 
 public class DivisibleByFive {
 
-    public static void main(String[] args) {
-        System.out.println(division(0));
-    }
-    public static boolean division(int n) {
+    public boolean isDivisible(int n) {
         boolean result = false;
         if (n % 5 == 0){
            result = true;
--- src/divisiblebyfive/DivisibleByFiveTest.java
+++ src/divisiblebyfive/DivisibleByFiveTest.java
@@ -8,47 +8,55 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DivisibleByFiveTest {
 
     private DivisibleByFive divisibleByFive;
+
     @BeforeEach
-    public void setup(){
-       divisibleByFive = new DivisibleByFive();
+    public void setup() {
+        divisibleByFive = new DivisibleByFive();
     }
 
     @Test
-    public void nothing(){}
+    public void nothing() {
+    }
 
     @Test
-    public void givenZero_returnTrue(){
-        boolean returnTrue = divisibleByFive.division(0);
+    public void givenZero_returnTrue() {
+        boolean returnTrue = divisibleByFive.isDivisible(0);
         assertEquals(true, returnTrue);
     }
 
     @Test
-    public void givenOne_returnsFalse(){
-        boolean returnFalse = divisibleByFive.division(1);
+    public void givenMinusOne_returnsFalse() {
+        boolean returnFalse = divisibleByFive.isDivisible(-1);
         assertEquals(false, returnFalse);
     }
 
     @Test
-    public void givenTwo_returnsFalse(){
-        boolean returnFalse = divisibleByFive.division(2);
+    public void givenOne_returnsFalse() {
+        boolean returnFalse = divisibleByFive.isDivisible(1);
         assertEquals(false, returnFalse);
     }
 
     @Test
-    public void givenFive_returnsFalse(){
-        boolean returnTrue = divisibleByFive.division(5);
+    public void givenTwo_returnsFalse() {
+        boolean returnFalse = divisibleByFive.isDivisible(2);
+        assertEquals(false, returnFalse);
+    }
+
+    @Test
+    public void givenFive_returnsFalse() {
+        boolean returnTrue = divisibleByFive.isDivisible(5);
         assertEquals(true, returnTrue);
     }
 
     @Test
-    public void givenNine_returnsFalse(){
-        boolean returnFalse = divisibleByFive.division(9);
+    public void givenNine_returnsFalse() {
+        boolean returnFalse = divisibleByFive.isDivisible(9);
         assertEquals(false, returnFalse);
     }
 
     @Test
-    public void givenMinusFiftyFive_returnsTrue(){
-        boolean returnTrue = divisibleByFive.division(-55);
+    public void givenMinusFiftyFive_returnsTrue() {
+        boolean returnTrue = divisibleByFive.isDivisible(-55);
         assertEquals(true, returnTrue);
     }
 }
