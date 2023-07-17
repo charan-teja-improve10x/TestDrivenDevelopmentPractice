0203008d988879a691bc158c87959664a88a80c9
CHARAN TEJA <charan.teja@improve10x.com>
2023-07-17 12:08:33 +0530

    created ReturnNextNUmberTest to find the next number

---
+++ src/returnnextnumber/ReturnNextNUmberTest.java
@@ -0,0 +1,55 @@
+package returnnextnumber;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ReturnNextNUmberTest {
+
+    @Test
+    public void nothing(){
+
+    }
+
+    @Test
+    public void givenZero_returnOne(){
+        ReturnNextNumber returnNextNumber = new ReturnNextNumber();
+        int nextNumber = returnNextNumber.findTheNextNumber(0);
+        assertEquals(1, nextNumber);
+    }
+
+    @Test
+    public void givenMinus1_returnZero(){
+        ReturnNextNumber returnNextNumber = new ReturnNextNumber();
+        int nextNumber = returnNextNumber.findTheNextNumber(-1);
+        assertEquals(0, nextNumber);
+    }
+
+    @Test
+    public void givenMinus5_returnMinus4(){
+        ReturnNextNumber returnNextNumber = new ReturnNextNumber();
+        int nextNumber = returnNextNumber.findTheNextNumber(-5);
+        assertEquals(-4, nextNumber);
+    }
+
+    @Test
+    public void given99_return100(){
+        ReturnNextNumber returnNextNumber = new ReturnNextNumber();
+        int nextNumber = returnNextNumber.findTheNextNumber(99);
+        assertEquals(100, nextNumber);
+    }
+
+    @Test
+    public void givenMinus99_returnMinus98(){
+        ReturnNextNumber returnNextNumber = new ReturnNextNumber();
+        int nextNumber = returnNextNumber.findTheNextNumber(-99);
+        assertEquals(-98, nextNumber);
+    }
+
+    @Test
+    public void givenMinus999_returnMinus1000(){
+        ReturnNextNumber returnNextNumber = new ReturnNextNumber();
+        int nextNumber = returnNextNumber.findTheNextNumber(999);
+        assertEquals(1000, nextNumber);
+    }
+}
---
+++ src/returnnextnumber/ReturnNextNumber.java
@@ -0,0 +1,9 @@
+package returnnextnumber;
+
+public class ReturnNextNumber {
+    public int findTheNextNumber(int number) {
+        int nextNumber = 0;
+        nextNumber = number + 1;
+        return nextNumber;
+    }
+}
