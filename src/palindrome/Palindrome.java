fb8eaf7be6bbe602af0100c07d3fc892e8b6ef6f
CHARAN TEJA <charan.teja@improve10x.com>
2023-07-17 16:46:48 +0530

    extracted a method in Palindrome

--- src/palindrome/Palindrome.java
+++ src/palindrome/Palindrome.java
@@ -11,15 +11,20 @@ public class Palindrome {
         } else if (name.length() == 1) {
             return true;
         } else if (name.length() >= 2) {
-            String reverse = "";
-            String trimWord = name.trim();
-            for (int i = trimWord.length() - 1; i >= 0; i--) {
-                reverse += trimWord.charAt(i);
-                if (trimWord.equals(reverse)) {
-                    return true;
-                }
-            }
+            if (reversingTheString(name)) return true;
         }
          return false;
+    }
+
+    private static boolean reversingTheString(String name) {
+        String reverse = "";
+        String trimWord = name.trim();
+        for (int i = trimWord.length() - 1; i >= 0; i--) {
+            reverse += trimWord.charAt(i);
+            if (trimWord.equals(reverse)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
