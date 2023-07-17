87e13a89e76d4123368efd1470b5a193e7a8ea2f
CHARAN TEJA <charan.teja@improve10x.com>
2023-07-17 11:03:53 +0530

    created NumbersEqualTest and it is done.

---
+++ src/numbersequal/NumbersEqual.java
@@ -0,0 +1,10 @@
+package numbersequal;
+
+public class NumbersEqual {
+    public boolean isEquals(int numOne, int numTwo) {
+        if (numOne == numTwo){
+            return true;
+        }
+        return false;
+    }
+}
