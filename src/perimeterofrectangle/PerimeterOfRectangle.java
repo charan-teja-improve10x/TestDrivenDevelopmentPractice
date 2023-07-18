453860c18b660d7d0b03d083564eaa198f316441
CHARAN TEJA <charan.teja@improve10x.com>
2023-07-18 10:41:57 +0530

    I did some code refactoring in PerimeterOfRectangle class

--- src/perimeterofrectangle/PerimeterOfRectangle.java
+++ src/perimeterofrectangle/PerimeterOfRectangle.java
@@ -2,14 +2,11 @@ package perimeterofrectangle;
 
 public class PerimeterOfRectangle {
 
-    public static void main(String[] args) {
-        System.out.println(findThePerimeterOfRectangle(-5,0));
-    }
-    public static int findThePerimeterOfRectangle(int height, int width) {
+    public int findThePerimeterOfRectangle(int height, int width) {
         int perimeter = 0;
-        if (height < 0 || width < 0 ){
+        if (height < 0 || width < 0) {
             perimeter = -1;
-        } else if (height == 0 && width == 0){
+        } else if (height == 0 && width == 0) {
             perimeter = 0;
         } else {
             perimeter = (height + width) * 2;
