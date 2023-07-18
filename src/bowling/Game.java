d7492eb273f4981494e4d31872021affd146560e
CHARAN TEJA <charan.teja@improve10x.com>
2023-07-18 21:23:51 +0530

    I created new package to practice the rewrote the test cases

--- src/bowling/Game.java
+++ src/bowling/Game.java
@@ -2,4 +2,42 @@ package bowling;
 
 public class Game {
 
+    private int[] rolls = new int[21];
+    private int currentRollIndex = 0;
+
+    public void roll(int pins) {
+        rolls[currentRollIndex] += pins;
+        currentRollIndex++;
+    }
+
+    public int score() {
+        int totalPins = 0;
+        int firstInFrame = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            if (isStrike(firstInFrame)){
+                totalPins += 10 + rolls[firstInFrame + 1] + rolls[firstInFrame + 2];
+                firstInFrame++;
+            } else if (isSpare(firstInFrame)) {
+                totalPins += 10 + firstInNextFrame(firstInFrame);
+                firstInFrame += 2;
+            } else{
+                totalPins += rolls[firstInFrame] + rolls[firstInFrame + 1];
+            firstInFrame += 2;
+            }
+        }
+        return totalPins;
+    }
+
+    private boolean isStrike(int firstInFrame) {
+        return rolls[firstInFrame] == 10;
+    }
+
+    private int firstInNextFrame(int firstInFrame) {
+        return rolls[firstInFrame + 2];
+    }
+
+    private boolean isSpare(int firstInFrame) {
+        return rolls[firstInFrame] + rolls[firstInFrame + 1] == 10;
+    }
 }
+
--- src/bowling/GameTest.java
+++ src/bowling/GameTest.java
@@ -1,5 +1,59 @@
 package bowling;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class GameTest {
 
+    private Game game;
+
+    private void rollMany(int totalRolls, int pins) {
+        for (int i = 0; i < totalRolls; i++){
+            game.roll(pins);
+        }
+    }
+    @BeforeEach
+    public void setup(){
+        game = new Game();
+    }
+
+    @Test
+    public void nothing(){}
+
+    @Test
+    public void canRoll(){
+        game.roll(1);
+    }
+
+    @Test
+    public void gutterGame(){
+        rollMany(20, 0);
+        assertEquals(0, game.score());
+    }
+
+    @Test
+    public void allOnes(){
+        rollMany(20, 1);
+        assertEquals(20, game.score());
+    }
+
+    @Test
+    public void oneSpare(){
+        game.roll(5);
+        game.roll(5);
+        game.roll(4);
+        rollMany(17,0);
+        assertEquals(18,game.score());
+    }
+
+    @Test
+    public void oneStrike(){
+        game.roll(10);
+        game.roll(4);
+        game.roll(2);
+        rollMany(16,0);
+        assertEquals(22,game.score());
+    }
 }
