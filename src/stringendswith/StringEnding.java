19e19305474d9c4624865ab332527b1cc9c3ea20
CHARAN TEJA <charan.teja@improve10x.com>
2023-07-18 11:05:56 +0530

    I did some code refactoring in StringEndsWithTest class

--- src/stringendswith/StringEnding.java
+++ src/stringendswith/StringEnding.java
@@ -3,9 +3,11 @@ package stringendswith;
 public class StringEnding {
     public boolean checkStringEnding(String name, String suffix) {
         boolean result = false;
-        if (name.endsWith(suffix)){
+        if (name == null) {
+            result = false;
+        } else if (name.trim().endsWith(suffix)) {
             result = true;
-        }else {
+        } else {
             result = false;
         }
         return result;
--- src/stringendswith/StringEndsWithTest.java
+++ src/stringendswith/StringEndsWithTest.java
@@ -10,40 +10,53 @@ public class StringEndsWithTest {
     private StringEnding stringEnding;
 
     @BeforeEach
-    public void setup(){
+    public void setup() {
         stringEnding = new StringEnding();
     }
 
     @Test
-    public void nothing(){}
+    public void nothing() {
+    }
+
+    @Test
+    public void givenNull_returnTrue() {
+        boolean returnTrue = stringEnding.checkStringEnding(null, null);
+        assertEquals(false, returnTrue);
+    }
+
+    @Test
+    public void givenEmpty_returnTrue() {
+        boolean returnTrue = stringEnding.checkStringEnding("", "");
+        assertEquals(true, returnTrue);
+    }
 
     @Test
     public void givenCharanAndAn_returnTrue() {
         boolean returnTrue = stringEnding.checkStringEnding("Charan", "an");
-        assertEquals( true, returnTrue);
+        assertEquals(true, returnTrue);
     }
 
     @Test
     public void givenCharanAndAnSpace_returnFalse() {
         boolean returnFalse = stringEnding.checkStringEnding("Charan", "an ");
-        assertEquals( false, returnFalse);
+        assertEquals(false, returnFalse);
     }
 
     @Test
     public void givenCharanAndCharan_returnTrue() {
         boolean returnTrue = stringEnding.checkStringEnding("Charan", "Charan");
-        assertEquals( true, returnTrue);
+        assertEquals(true, returnTrue);
     }
 
     @Test
     public void givenCharanTejaAndTeja_returnTrue() {
-        boolean returnTrue = stringEnding.checkStringEnding("Charan Teja", " Teja");
-        assertEquals( true, returnTrue);
+        boolean returnTrue = stringEnding.checkStringEnding("Charan Teja   ", " Teja");
+        assertEquals(true, returnTrue);
     }
 
     @Test
     public void givenCharanTejaAndTeja_returnFalse() {
-        boolean returnFalse = stringEnding.checkStringEnding("Charan Teja", " Teja  ");
-        assertEquals( false, returnFalse);
+        boolean returnFalse = stringEnding.checkStringEnding("Charan Teja   ", " Teja  ");
+        assertEquals(false, returnFalse);
     }
 }
