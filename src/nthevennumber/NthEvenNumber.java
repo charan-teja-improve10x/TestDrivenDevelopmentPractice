f0d0bce1c4f6662c18d923aeb89a41cfe397ec5c
CHARAN TEJA <charan.teja@improve10x.com>
2023-07-18 10:27:38 +0530

    I did some code refactoring in NthEvenNumberTest class

--- src/nthevennumber/NthEvenNumber.java
+++ src/nthevennumber/NthEvenNumber.java
@@ -3,10 +3,12 @@ package nthevennumber;
 public class NthEvenNumber {
     public int findTheNthEvenNumber(int number) {
         int result = 0;
-        if (number == 0 || number == 1){
+        if (number < 0){
+            result= -2;
+        } else if (number == 0 || number == 1) {
             result = 0;
         } else {
-            result = (number-1)*2;
+            result = (number - 1) * 2;
         }
         return result;
     }
--- src/nthevennumber/NthEvenNumberTest.java
+++ src/nthevennumber/NthEvenNumberTest.java
@@ -10,45 +10,52 @@ public class NthEvenNumberTest {
     private NthEvenNumber nthEvenNumber;
 
     @BeforeEach
-    public void setup(){
+    public void setup() {
         nthEvenNumber = new NthEvenNumber();
     }
 
     @Test
-    public void nothing(){}
+    public void nothing() {
+    }
 
     @Test
-    public void givenZero_returnZero(){
+    public void givenZero_returnZero() {
         int evenNumber = nthEvenNumber.findTheNthEvenNumber(0);
         assertEquals(0, evenNumber);
     }
 
     @Test
-    public void givenOne_returnZero(){
+    public void givenNegative_returnNegative() {
+        int evenNumber = nthEvenNumber.findTheNthEvenNumber(-5);
+        assertEquals(-2, evenNumber);
+    }
+
+    @Test
+    public void givenOne_returnZero() {
         int evenNumber = nthEvenNumber.findTheNthEvenNumber(1);
         assertEquals(0, evenNumber);
     }
 
     @Test
-    public void given2_return2(){
+    public void given2_return2() {
         int evenNumber = nthEvenNumber.findTheNthEvenNumber(2);
         assertEquals(2, evenNumber);
     }
 
     @Test
-    public void given3_return4(){
+    public void given3_return4() {
         int evenNumber = nthEvenNumber.findTheNthEvenNumber(3);
         assertEquals(4, evenNumber);
     }
 
     @Test
-    public void given10_return18(){
+    public void given10_return18() {
         int evenNumber = nthEvenNumber.findTheNthEvenNumber(10);
         assertEquals(18, evenNumber);
     }
 
     @Test
-    public void given99_return196(){
+    public void given99_return196() {
         int evenNumber = nthEvenNumber.findTheNthEvenNumber(99);
         assertEquals(196, evenNumber);
     }
