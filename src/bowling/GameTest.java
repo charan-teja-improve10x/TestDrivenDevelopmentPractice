8925ce57b69b26d8c7ca4c62e59a8684a9c15aa7
CHARAN TEJA <charan.teja@improve10x.com>
2023-07-19 11:32:41 +0530

    created new test case to calculate the bowling test

--- src/bowling/GameTest.java
+++ src/bowling/GameTest.java
@@ -105,4 +105,29 @@ public class GameTest {
         game.roll(2);
         assertEquals(114, game.score());
     }
+
+    @Test
+    public void charanScoreBoard(){
+        game.roll(7);
+        game.roll(2);
+        game.roll(10);
+        game.roll(6);
+        game.roll(3);
+        game.roll(2);
+        game.roll(5);
+        game.roll(8);
+        game.roll(1);
+        game.roll(4);
+        game.roll(5);
+        game.roll(6);
+        game.roll(4);
+        game.roll(7);
+        game.roll(2);
+        game.roll(4);
+        game.roll(2);
+        game.roll(5);
+        game.roll(5);
+        game.roll(2);
+        assertEquals(106, game.score());
+    }
 }
