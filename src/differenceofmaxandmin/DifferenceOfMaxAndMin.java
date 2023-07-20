562b8c6efc264fbb9d1f5e7344294b7cbddac933
CHARAN TEJA <charan.teja@improve10x.com>
2023-07-20 11:41:26 +0530

    I did Some code refactoring in DifferenceOfMaxAndMinTest and DifferenceOfMaxAndMin classes

--- src/differenceofmaxandmin/DifferenceOfMaxAndMin.java
+++ src/differenceofmaxandmin/DifferenceOfMaxAndMin.java
@@ -3,20 +3,26 @@ package differenceofmaxandmin;
 public class DifferenceOfMaxAndMin {
     public int difference(int[] numbers) {
         int result = 0;
-        if (numbers.length == 1){
+        if (numbers.length == 1) {
             return numbers[0];
         } else if (numbers.length > 1) {
-            int min = numbers[0];
-            int max = numbers[0];
-            for (int i = 0; i < numbers.length; i++){
-                if (numbers[i] > max){
-                    max = numbers[i];
-                } else if (numbers[i]< min) {
-                    min = numbers[i];
-                }
-            }
-            result = max - min;
+            result = getMaxMinDifference(numbers);
         }
+        return result;
+    }
+
+    private static int getMaxMinDifference(int[] numbers) {
+        int result;
+        int min = numbers[0];
+        int max = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            } else if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+        result = max - min;
         return result;
     }
 }
--- src/differenceofmaxandmin/DifferenceOfMaxAndMinTest.java
+++ src/differenceofmaxandmin/DifferenceOfMaxAndMinTest.java
@@ -10,63 +10,64 @@ public class DifferenceOfMaxAndMinTest {
     private DifferenceOfMaxAndMin differenceOfMaxAndMin;
 
     @BeforeEach
-    public void setup(){
+    public void setup() {
         differenceOfMaxAndMin = new DifferenceOfMaxAndMin();
     }
 
     @Test
-    public void nothing(){}
+    public void nothing() {
+    }
 
     @Test
-    public void givenEmptyArray_returnZero(){
+    public void givenEmptyArray_returnZero() {
         int result = differenceOfMaxAndMin.difference(new int[]{});
         assertEquals(result, 0);
     }
 
     @Test
-    public void givenOneElement_returnElement(){
+    public void givenOneElement_returnElement() {
         int result = differenceOfMaxAndMin.difference(new int[]{1});
         assertEquals(result, 1);
     }
 
     @Test
-    public void given01_return1(){
+    public void given01_return1() {
         int result = differenceOfMaxAndMin.difference(new int[]{0, 1});
         assertEquals(result, 1);
     }
 
     @Test
-    public void given0Minus1_return1(){
+    public void given0Minus1_return1() {
         int result = differenceOfMaxAndMin.difference(new int[]{0, -1});
         assertEquals(result, 1);
     }
 
     @Test
-    public void given11_return0(){
+    public void given11_return0() {
         int result = differenceOfMaxAndMin.difference(new int[]{1, 1});
         assertEquals(result, 0);
     }
 
     @Test
-    public void given12_return1(){
+    public void given12_return1() {
         int result = differenceOfMaxAndMin.difference(new int[]{1, 2});
         assertEquals(result, 1);
     }
 
     @Test
-    public void given123_return2(){
+    public void given123_return2() {
         int result = differenceOfMaxAndMin.difference(new int[]{1, 2, 3});
         assertEquals(result, 2);
     }
 
     @Test
-    public void given12311_return10(){
+    public void given12311_return10() {
         int result = differenceOfMaxAndMin.difference(new int[]{1, 2, 3, 11});
         assertEquals(result, 10);
     }
 
     @Test
-    public void given1213143_return11(){
+    public void given1213143_return11() {
         int result = differenceOfMaxAndMin.difference(new int[]{12, 13, 14, 3});
         assertEquals(result, 11);
     }
