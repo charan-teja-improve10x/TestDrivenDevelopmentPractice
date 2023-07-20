190f8a0b48d46fe85ec379f9bfc0bd9ed1596db9
CHARAN TEJA <charan.teja@improve10x.com>
2023-07-20 14:20:51 +0530

    I did some code refactoring in BombTes

--- src/findthebomb/BombTest.java
+++ src/findthebomb/BombTest.java
@@ -1,23 +1,29 @@
 package findthebomb;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BombTest {
 
+    private Bomb bomb;
+
+    @BeforeEach
+    public void setup(){
+        bomb = new Bomb();
+    }
+
     @Test
     public void nothing(){}
 
     @Test
     public void givenNull_returnEmpty(){
-        Bomb bomb = new Bomb();
         String result = bomb.findBomb(null);
         assertEquals("", result);
     }
     @Test
     public void givenEmpty_returnEmpty(){
-        Bomb bomb = new Bomb();
         String result = bomb.findBomb(null);
         assertEquals("", result);
     }
