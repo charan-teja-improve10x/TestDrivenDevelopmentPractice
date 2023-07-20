8297002ccceeb3a798371995699bb0aa5fc88b40
CHARAN TEJA <charan.teja@improve10x.com>
2023-07-20 14:14:33 +0530

    I added two  test cases for finding the bomb program.

---
+++ src/findthebomb/Bomb.java
@@ -0,0 +1,11 @@
+package findthebomb;
+
+public class Bomb {
+    public String findBomb(String name) {
+        String result = "";
+        if (name == null || name == ""){
+            result = "";
+        }
+        return result;
+    }
+}
--- src/findthebomb/BombTest.java
+++ src/findthebomb/BombTest.java
@@ -1,4 +1,24 @@
 package findthebomb;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BombTest {
+
+    @Test
+    public void nothing(){}
+
+    @Test
+    public void givenNull_returnEmpty(){
+        Bomb bomb = new Bomb();
+        String result = bomb.findBomb(null);
+        assertEquals("", result);
+    }
+    @Test
+    public void givenEmpty_returnEmpty(){
+        Bomb bomb = new Bomb();
+        String result = bomb.findBomb(null);
+        assertEquals("", result);
+    }
 }
