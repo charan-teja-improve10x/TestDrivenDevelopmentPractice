710b75dc31271cb402b30e051f263367ee2aa369
CHARAN TEJA <charan.teja@improve10x.com>
2023-07-21 22:28:34 +0530

    I created Coin Class and tested the test cases

---
+++ src/templerun/Coin.java
@@ -0,0 +1,18 @@
+package templerun;
+
+public class Coin {
+    int value;
+    public Coin(int value) {
+        if (value < 0 || value > 100 ) {
+            throw new InvalidCoinValueException();
+        }
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public class InvalidCoinValueException extends RuntimeException{
+    }
+}
