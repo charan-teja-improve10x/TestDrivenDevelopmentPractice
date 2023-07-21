0f4066422e6a9be382382c87659b25bb3f68ffaf
CHARAN TEJA <charan.teja@improve10x.com>
2023-07-21 22:06:54 +0530

    I created PlayerTest class and passed some test cases.

--- src/templerun/Player.java
+++ src/templerun/Player.java
@@ -1,38 +1,38 @@
 package templerun;
 
-public class Player extends Character {
+import java.util.Random;
+
+public class Player  {
     String name;
-    int score;
     int health;
-    public Player(String name, int health){
-        super(name, health);
-
+    public Player(String name) {
+        this.name = name == null? "" : name.trim();
+        this.health = 100;
     }
 
-    public Player(String name){
-        super(name);
-
-    }
-
-    public Player(int health){
-        super(health);
-
-    }
-
-    @Override
-    public String getName() {
-        if (name == null || name == ""){
-            return "";
+    public Player(String name, int health) {
+        if (health < 0 || health > 100) {
+            throw new InvalidHealthException();
         }
+        this.health = health;
+    }
+
+    public String getName() {
         return name;
     }
 
-    public int getScore() {
-        return score;
-    }
-
-    @Override
     public int getHealth() {
         return health;
+    }
+
+    public void run() {
+        System.out.println("Running...!");
+    }
+
+    public int getScore() {
+        return 0;
+    }
+
+    public class InvalidHealthException extends RuntimeException {
     }
 }
