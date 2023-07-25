99189d0f9b04afa089d3c433093a92f510ef61b9
CHARAN TEJA <charan.teja@improve10x.com>
2023-07-25 12:29:28 +0530

    I created Obstacle class and test class

--- src/templerun/Obstacle.java
+++ src/templerun/Obstacle.java
@@ -5,13 +5,28 @@ public class Obstacle {
     protected String name;
     protected int damage;
 
-    public Obstacle(String name, int damage) {
-        this.name = name;
+    public Obstacle(String name, int damage) throws InvalidObstacleDamageException {
         this.damage = damage;
+        this.name = name == null ? "" : name.trim();
+        if (damage < 0 || damage > 100) {
+            throw new InvalidObstacleDamageException();
+        }
+
     }
 
     public void collide(Player player){
         System.out.println(name + " has Collide with " + player.getName() + " and caused " + damage + " damage.");
         player.healthDamage(damage);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public class InvalidObstacleDamageException extends Exception {
     }
 }
