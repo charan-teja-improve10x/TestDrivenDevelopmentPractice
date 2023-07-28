58475168b14a0b34388fd4a024ff0ae4c097dfa9
CHARAN TEJA <charan.teja@improve10x.com>
2023-07-28 15:14:46 +0530

    I did Primorial program by using Tdd

--- src/primorial/Primorial.java
+++ src/primorial/Primorial.java
@@ -2,6 +2,30 @@ package primorial;
 
 public class Primorial {
     public long findThePrimorial(int n) {
-        return 0;
+        int primorial = 1;
+        if (n < 0) {
+            primorial = -1;
+        } else if (n == 0) {
+            primorial = 0;
+        } else {
+            int primesCount = 0;
+            for (int i = 1; i < Integer.MAX_VALUE; i++) {
+                if (primesCount != n) {
+                    int factors = 0;
+                    for (int j = 1; j <= i; j++) {
+                        if (i % j == 0) {
+                            factors++;
+                        }
+                    }
+                    if (factors == 2) {
+                        primesCount++;
+                        primorial *= i;
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+        return primorial;
     }
 }
--- src/primorial/PrimorialTest.java
+++ src/primorial/PrimorialTest.java
@@ -22,4 +22,46 @@ public class PrimorialTest {
         long product = primorial.findThePrimorial(-1);
         assertEquals(-1, product);
     }
+
+    @Test
+    public void givenOne_returnTwo(){
+        Primorial primorial = new Primorial();
+        long product = primorial.findThePrimorial(1);
+        assertEquals(2, product);
+    }
+
+    @Test
+    public void given2_return6(){
+        Primorial primorial = new Primorial();
+        long product = primorial.findThePrimorial(2);
+        assertEquals(6, product);
+    }
+
+    @Test
+    public void given3_return30(){
+        Primorial primorial = new Primorial();
+        long product = primorial.findThePrimorial(2);
+        assertEquals(6, product);
+    }
+
+    @Test
+    public void given4_return210(){
+        Primorial primorial = new Primorial();
+        long product = primorial.findThePrimorial(4);
+        assertEquals(210, product);
+    }
+
+    @Test
+    public void given5_return210(){
+        Primorial primorial = new Primorial();
+        long product = primorial.findThePrimorial(5);
+        assertEquals(2310, product);
+    }
+
+    @Test
+    public void given8_return9699690(){
+        Primorial primorial = new Primorial();
+        long product = primorial.findThePrimorial(8);
+        assertEquals(9699690, product);
+    }
 }
