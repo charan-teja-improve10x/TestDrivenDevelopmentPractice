5872f20cc51ad8ac494db1028c4b90dd8eff3a7c
CHARAN TEJA <charan.teja@improve10x.com>
2023-08-05 10:38:19 +0530

    I did some refactoring in test cases.

--- src/largestnumber/LargestNumberTest.java
+++ src/largestnumber/LargestNumberTest.java
@@ -1,10 +1,18 @@
 package largestnumber;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LargestNumberTest {
+
+    private LargestNumber largestNumber;
+
+    @BeforeEach
+    public void setup(){
+        largestNumber = new LargestNumber();
+    }
 
     @Test
     public void nothing(){}
@@ -12,49 +20,48 @@ public class LargestNumberTest {
 
     @Test
     public void givenNull_returnZero(){
-        LargestNumber largestNumber = new LargestNumber();
         int largeNumber = largestNumber.findTheLargeNUmber(null);
         assertEquals(0, largeNumber);
     }
     @Test
     public void givenEmpty_returnZero(){
-        LargestNumber largestNumber = new LargestNumber();
         int largeNumber = largestNumber.findTheLargeNUmber(new int[]{});
         assertEquals(0, largeNumber);
     }
 
     @Test
     public void given0_returnZero(){
-        LargestNumber largestNumber = new LargestNumber();
         int largeNumber = largestNumber.findTheLargeNUmber(new int[]{0});
         assertEquals(0, largeNumber);
     }
 
     @Test
     public void given1_return1(){
-        LargestNumber largestNumber = new LargestNumber();
         int largeNumber = largestNumber.findTheLargeNUmber(new int[]{1});
         assertEquals(1, largeNumber);
     }
 
     @Test
     public void given12_return2(){
-        LargestNumber largestNumber = new LargestNumber();
         int largeNumber = largestNumber.findTheLargeNUmber(new int[]{1, 2});
         assertEquals(2, largeNumber);
     }
 
     @Test
     public void given132_return2(){
-        LargestNumber largestNumber = new LargestNumber();
         int largeNumber = largestNumber.findTheLargeNUmber(new int[]{1, 3, 2});
         assertEquals(3, largeNumber);
     }
 
     @Test
     public void given13245101_return10(){
-        LargestNumber largestNumber = new LargestNumber();
         int largeNumber = largestNumber.findTheLargeNUmber(new int[]{1, 3, 2, 4, 5, 10, 1});
         assertEquals(10, largeNumber);
+    }
+
+    @Test
+    public void given132451011318_return10(){
+        int largeNumber = largestNumber.findTheLargeNUmber(new int[]{1, 3, 2, 4, 5, 10, 1, 13, 18});
+        assertEquals(18, largeNumber);
     }
 }
