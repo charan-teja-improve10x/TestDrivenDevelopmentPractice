8a1eea653067e1e2f4c61707a30d1095d0a9d346
CHARAN TEJA <charan.teja@improve10x.com>
2023-08-05 22:55:06 +0530

    I wrote test case for two same elements.

--- src/removeduplicates/RemoveDuplicates.java
+++ src/removeduplicates/RemoveDuplicates.java
@@ -1,10 +1,20 @@
 package removeduplicates;
 
 public class RemoveDuplicates {
-    public int removeDuplicatesAndReturnTheLength(int[] numbers) {
+    public int removeDuplicatesAndReturnTheLength(int[] numbers, int n) {
         if (numbers == null || numbers.length == 0){
             return 0;
+        }else {
+             n = numbers.length;
+            int i = 0;
+            for (int j = 1; j < numbers.length; j++){
+                if (numbers[i] != numbers[j]){
+                    i++;
+                    numbers[i] = numbers[j];
+                }
+            }
+            n = i + 1;
         }
-        return numbers.length;
+        return n;
     }
 }
--- src/removeduplicates/RemoveDuplicatesTest.java
+++ src/removeduplicates/RemoveDuplicatesTest.java
@@ -1,6 +1,5 @@
 package removeduplicates;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,36 +12,35 @@ public class RemoveDuplicatesTest {
     @Test
     public void givenNull_returnZero(){
         RemoveDuplicates removeDuplicates = new RemoveDuplicates();
-        int length = removeDuplicates.removeDuplicatesAndReturnTheLength(null);
+        int length = removeDuplicates.removeDuplicatesAndReturnTheLength(null, 0);
         assertEquals(0, length);
     }
 
     @Test
     public void givenEmpty_returnZero(){
         RemoveDuplicates removeDuplicates = new RemoveDuplicates();
-        int length = removeDuplicates.removeDuplicatesAndReturnTheLength(new int[]{});
+        int length = removeDuplicates.removeDuplicatesAndReturnTheLength(new int[]{}, 0);
         assertEquals(0, length);
     }
 
     @Test
     public void givenOneElement_returnOne(){
         RemoveDuplicates removeDuplicates = new RemoveDuplicates();
-        int length = removeDuplicates.removeDuplicatesAndReturnTheLength(new int[]{0});
+        int length = removeDuplicates.removeDuplicatesAndReturnTheLength(new int[]{0}, 1);
         assertEquals(1, length);
     }
 
-    @Disabled
     @Test
     public void givenOneTwo_returnTwo(){
         RemoveDuplicates removeDuplicates = new RemoveDuplicates();
-        int length = removeDuplicates.removeDuplicatesAndReturnTheLength(new int[]{1, 2});
+        int length = removeDuplicates.removeDuplicatesAndReturnTheLength(new int[]{1, 2}, 2);
         assertEquals(2, length);
     }
 
     @Test
     public void givenTwoOnes_returnOne(){
         RemoveDuplicates removeDuplicates = new RemoveDuplicates();
-        int length = removeDuplicates.removeDuplicatesAndReturnTheLength(new int[]{1, 1});
+        int length = removeDuplicates.removeDuplicatesAndReturnTheLength(new int[]{1, 1}, 2);
         assertEquals(1, length);
     }
 }
