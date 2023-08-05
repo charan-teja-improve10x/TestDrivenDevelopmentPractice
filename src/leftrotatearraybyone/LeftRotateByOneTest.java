ebfb89bc72302cda6981e601dd093f08ca2ad053
CHARAN TEJA <charan.teja@improve10x.com>
2023-08-05 23:44:28 +0530

    I did some code refactoring in LeftRotateByOneTest class

--- src/leftrotatearraybyone/LeftRotateByOneTest.java
+++ src/leftrotatearraybyone/LeftRotateByOneTest.java
@@ -1,59 +1,59 @@
 package leftrotatearraybyone;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class LeftRotateByOneTest {
 
+    private LeftRotateByOne leftRotateByOne;
+
+    @BeforeEach
+    public void setup(){
+        leftRotateByOne = new LeftRotateByOne();
+    }
     @Test
     public void nothing(){}
 
     @Test
     public void givenNull_returnEmpty(){
-        LeftRotateByOne leftRotateByOne = new LeftRotateByOne();
         int[] rotatedArray = leftRotateByOne.leftRotateArrayByOne(null, 0);
         assertArrayEquals(new int[]{}, rotatedArray);
     }
 
     @Test
     public void givenEmpty_returnEmpty(){
-        LeftRotateByOne leftRotateByOne = new LeftRotateByOne();
         int[] rotatedArray = leftRotateByOne.leftRotateArrayByOne(new int[]{}, 0);
         assertArrayEquals(new int[]{}, rotatedArray);
     }
 
     @Test
     public void givenOneElement_returnOne(){
-        LeftRotateByOne leftRotateByOne = new LeftRotateByOne();
         int[] rotatedArray = leftRotateByOne.leftRotateArrayByOne(new int[]{1}, 1);
         assertArrayEquals(new int[]{1}, rotatedArray);
     }
 
     @Test
     public void givenMinusOne_returnMinusOne(){
-        LeftRotateByOne leftRotateByOne = new LeftRotateByOne();
         int[] rotatedArray = leftRotateByOne.leftRotateArrayByOne(new int[]{-1}, 1);
         assertArrayEquals(new int[]{-1}, rotatedArray);
     }
 
     @Test
     public void givenMinusOneMinusTwo_returnMinusTwoOne(){
-        LeftRotateByOne leftRotateByOne = new LeftRotateByOne();
         int[] rotatedArray = leftRotateByOne.leftRotateArrayByOne(new int[]{-1, -2}, 2);
         assertArrayEquals(new int[]{-2, -1}, rotatedArray);
     }
 
     @Test
     public void givenOneTwo_returnTwoOne(){
-        LeftRotateByOne leftRotateByOne = new LeftRotateByOne();
         int[] rotatedArray = leftRotateByOne.leftRotateArrayByOne(new int[]{1, 2}, 2);
         assertArrayEquals(new int[]{2, 1}, rotatedArray);
     }
 
     @Test
     public void givenOneTwoOne_returnTwoOne(){
-        LeftRotateByOne leftRotateByOne = new LeftRotateByOne();
         int[] rotatedArray = leftRotateByOne.leftRotateArrayByOne(new int[]{1, 2, 1}, 3);
         assertArrayEquals(new int[]{2, 1, 1}, rotatedArray);
     }
