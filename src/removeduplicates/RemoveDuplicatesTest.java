fe5514e73954190bc8f322e274b6499ca955fcb4
CHARAN TEJA <charan.teja@improve10x.com>
2023-08-05 22:46:21 +0530

    I wrote test case for two elements

--- src/removeduplicates/RemoveDuplicatesTest.java
+++ src/removeduplicates/RemoveDuplicatesTest.java
@@ -1,5 +1,6 @@
 package removeduplicates;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -27,6 +28,21 @@ public class RemoveDuplicatesTest {
     public void givenOneElement_returnOne(){
         RemoveDuplicates removeDuplicates = new RemoveDuplicates();
         int length = removeDuplicates.removeDuplicatesAndReturnTheLength(new int[]{0});
+        assertEquals(1, length);
+    }
+
+    @Disabled
+    @Test
+    public void givenOneTwo_returnTwo(){
+        RemoveDuplicates removeDuplicates = new RemoveDuplicates();
+        int length = removeDuplicates.removeDuplicatesAndReturnTheLength(new int[]{1, 2});
+        assertEquals(2, length);
+    }
+
+    @Test
+    public void givenTwoOnes_returnOne(){
+        RemoveDuplicates removeDuplicates = new RemoveDuplicates();
+        int length = removeDuplicates.removeDuplicatesAndReturnTheLength(new int[]{1, 1});
         assertEquals(1, length);
     }
 }
