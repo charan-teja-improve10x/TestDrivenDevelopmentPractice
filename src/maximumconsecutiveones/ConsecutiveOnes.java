2cdf75f4af2c9a45beefc28a791553aa07c705e9
CHARAN TEJA <charan.teja@improve10x.com>
2023-08-06 16:25:36 +0530

    I did some test cases to find the ConsecutiveOnes maximum count

--- src/maximumconsecutiveones/ConsecutiveOnes.java
+++ src/maximumconsecutiveones/ConsecutiveOnes.java
@@ -2,6 +2,20 @@ package maximumconsecutiveones;
 
 public class ConsecutiveOnes {
     public int countOfConsecutiveOnes(int[] numbers) {
-        return 0;
+        if (numbers == null || numbers.length == 0){
+            return 0;
+        }
+
+        int maxi = 0;
+        int count = 0;
+        for (int i = 0; i < numbers.length; i++){
+            if (numbers[i] == 1){
+                count++;
+            }else {
+                count = 0;
+            }
+            maxi = Math.max(maxi, count);
+        }
+        return maxi;
     }
 }
--- src/maximumconsecutiveones/ConsecutiveOnesTest.java
+++ src/maximumconsecutiveones/ConsecutiveOnesTest.java
@@ -30,5 +30,38 @@ public class ConsecutiveOnesTest {
         assertEquals(0,maxi);
     }
 
+    @Test
+    public void givenOne_returnOne(){
+        ConsecutiveOnes consecutiveOnes = new ConsecutiveOnes();
+        int maxi = consecutiveOnes.countOfConsecutiveOnes(new int[]{1});
+        assertEquals(1,maxi);
+    }
 
+    @Test
+    public void givenTwoOnes_returnTwo(){
+        ConsecutiveOnes consecutiveOnes = new ConsecutiveOnes();
+        int maxi = consecutiveOnes.countOfConsecutiveOnes(new int[]{1, 1});
+        assertEquals(2,maxi);
+    }
+
+    @Test
+    public void given1011_returnTwo(){
+        ConsecutiveOnes consecutiveOnes = new ConsecutiveOnes();
+        int maxi = consecutiveOnes.countOfConsecutiveOnes(new int[]{1, 0, 1, 1});
+        assertEquals(2,maxi);
+    }
+
+    @Test
+    public void given0022_returnZero(){
+        ConsecutiveOnes consecutiveOnes = new ConsecutiveOnes();
+        int maxi = consecutiveOnes.countOfConsecutiveOnes(new int[]{0, 0, 2, 2});
+        assertEquals(0,maxi);
+    }
+
+    @Test
+    public void given101110011111_returnTwo(){
+        ConsecutiveOnes consecutiveOnes = new ConsecutiveOnes();
+        int maxi = consecutiveOnes.countOfConsecutiveOnes(new int[]{1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1});
+        assertEquals(5,maxi);
+    }
 }
