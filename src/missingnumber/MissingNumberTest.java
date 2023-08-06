93aa16d3508bc346d3c42d7df5d68e48589008f3
CHARAN TEJA <charan.teja@improve10x.com>
2023-08-06 12:18:37 +0530

    I did some code refactoring in MissingNumberTest

--- src/missingnumber/MissingNumberTest.java
+++ src/missingnumber/MissingNumberTest.java
@@ -1,60 +1,62 @@
 package missingnumber;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MissingNumberTest {
 
-    @Test
-    public void nothing(){}
+    private MissingNumberArray missingNumberArray;
+
+    @BeforeEach
+    public void setup() {
+        missingNumberArray = new MissingNumberArray();
+    }
 
     @Test
-    public void givenNull_return0(){
-        MissingNumberArray missingNumberArray = new MissingNumberArray();
+    public void nothing() {
+    }
+
+    @Test
+    public void givenNull_return0() {
         int missedNumber = missingNumberArray.findTheMissingNumber(null, 0);
         assertEquals(0, missedNumber);
     }
 
     @Test
-    public void givenEmpty_return0(){
-        MissingNumberArray missingNumberArray = new MissingNumberArray();
+    public void givenEmpty_return0() {
         int missedNumber = missingNumberArray.findTheMissingNumber(new int[]{}, 0);
         assertEquals(0, missedNumber);
     }
 
     @Test
-    public void givenOneElement_returnElement(){
-        MissingNumberArray missingNumberArray = new MissingNumberArray();
+    public void givenOneElement_returnElement() {
         int missedNumber = missingNumberArray.findTheMissingNumber(new int[]{1}, 1);
         assertEquals(1, missedNumber);
     }
 
     @Test
-    public void given4_return4(){
-        MissingNumberArray missingNumberArray = new MissingNumberArray();
+    public void given4_return4() {
         int missedNumber = missingNumberArray.findTheMissingNumber(new int[]{4}, 1);
         assertEquals(4, missedNumber);
     }
 
     @Test
-    public void given13_return2(){
-        MissingNumberArray missingNumberArray = new MissingNumberArray();
+    public void given13_return2() {
         int missedNumber = missingNumberArray.findTheMissingNumber(new int[]{1, 3}, 2);
         assertEquals(2, missedNumber);
     }
 
     @Test
-    public void given1235_return4(){
-        MissingNumberArray missingNumberArray = new MissingNumberArray();
-        int missedNumber = missingNumberArray.findTheMissingNumber(new int[]{1, 2,  3, 5}, 4);
+    public void given1235_return4() {
+        int missedNumber = missingNumberArray.findTheMissingNumber(new int[]{1, 2, 3, 5}, 4);
         assertEquals(4, missedNumber);
     }
 
     @Test
-    public void given1234567810_return2(){
-        MissingNumberArray missingNumberArray = new MissingNumberArray();
-        int missedNumber = missingNumberArray.findTheMissingNumber(new int[]{1, 2,  3, 4, 5, 6, 7, 9, 10}, 9);
+    public void given1234567810_return2() {
+        int missedNumber = missingNumberArray.findTheMissingNumber(new int[]{1, 2, 3, 4, 5, 6, 7, 9, 10}, 9);
         assertEquals(8, missedNumber);
     }
 }
