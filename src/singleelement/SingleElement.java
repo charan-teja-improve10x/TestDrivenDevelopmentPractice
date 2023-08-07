9d31c3549ff2fbc3bd02ff5dad7fd8437a579ebb
CHARAN TEJA <charan.teja@improve10x.com>
2023-08-07 11:03:58 +0530

    I did test cases for getting the single number in an array.

--- src/singleelement/SingleElement.java
+++ src/singleelement/SingleElement.java
@@ -5,6 +5,20 @@ public class SingleElement {
         if (arr == null || arr.length == 0){
             return -1;
         }
-        return arr[0];
+
+        int n = arr.length;
+        for (int i = 0; i < n; i++){
+            int num = arr[i];
+            int count = 0;
+            for (int j = 0; j<n; j++){
+                if (arr[j] == num){
+                    count++;
+                }
+            }
+            if (count == 1){
+                return num;
+            }
+        }
+        return -1;
     }
 }
--- src/singleelement/SingleElementTest.java
+++ src/singleelement/SingleElementTest.java
@@ -29,4 +29,46 @@ public class SingleElementTest {
         int singlesCount = singleElement.getSingleElementCount(new int[]{0});
         assertEquals(0, singlesCount);
     }
+
+    @Test
+    public void givenOneOne_returnOne(){
+        SingleElement singleElement = new SingleElement();
+        int singlesCount = singleElement.getSingleElementCount(new int[]{1});
+        assertEquals(1, singlesCount);
+    }
+
+    @Test
+    public void givenOneOne_returnMinusOne(){
+        SingleElement singleElement = new SingleElement();
+        int singlesCount = singleElement.getSingleElementCount(new int[]{1, 1});
+        assertEquals(-1, singlesCount);
+    }
+
+    @Test
+    public void givenOneOne2_returnMinusOne(){
+        SingleElement singleElement = new SingleElement();
+        int singlesCount = singleElement.getSingleElementCount(new int[]{1, 1, 2});
+        assertEquals(2, singlesCount);
+    }
+
+    @Test
+    public void givenOne2_returnMinusOne(){
+        SingleElement singleElement = new SingleElement();
+        int singlesCount = singleElement.getSingleElementCount(new int[]{1, 2, 2});
+        assertEquals(1, singlesCount);
+    }
+
+    @Test
+    public void given1122336446557_returnMinusOne(){
+        SingleElement singleElement = new SingleElement();
+        int singlesCount = singleElement.getSingleElementCount(new int[]{1, 1, 2, 2, 3, 3, 6, 4, 4, 5, 5, 6, 7});
+        assertEquals(7, singlesCount);
+    }
+
+    @Test
+    public void givenAllNumbersPairs_returnMinusOne(){
+        SingleElement singleElement = new SingleElement();
+        int singlesCount = singleElement.getSingleElementCount(new int[]{1, 1, 2, 2, 3, 3, 6, 4, 4, 5, 5, 6, 7, 7});
+        assertEquals(-1, singlesCount);
+    }
 }
