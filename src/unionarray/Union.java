9fa6d478685507f482f74edd206c2235141acc49
CHARAN TEJA <charan.teja@improve10x.com>
2023-08-07 15:35:06 +0530

    I did test cases for UnionArray.

--- src/unionarray/Union.java
+++ src/unionarray/Union.java
@@ -1,10 +1,29 @@
 package unionarray;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 
 public class Union {
     public ArrayList<Integer> findUnion(Integer[] num, Integer[] integers) {
         ArrayList<Integer> union = new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
+        if (num == null && integers == null || num.length == 0 && integers.length == 0){
+            return new ArrayList<>();
+        }
+        int n = num.length;
+        for (int i = 0; i < n; i++){
+            set.add(num[i]);
+        }
+
+        int m = integers.length;
+        for (int i = 0; i < m; i++){
+            set.add(integers[i]);
+        }
+
+        for (int u: set) {
+           union.add(u);
+        }
         return union;
     }
 }
--- src/unionarray/UnionTest.java
+++ src/unionarray/UnionTest.java
@@ -1,10 +1,11 @@
 package unionarray;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UnionTest {
 
@@ -26,16 +27,56 @@ public class UnionTest {
     }
 
     @Test
-    public void givenOneNUllAndOneEmpty_returnEmptyList(){
+    public void givenNullANd0_return0(){
         Union union = new Union();
-        ArrayList<Integer> unionArray = union.findUnion(null, new Integer[]{});
-        assertEquals(new ArrayList<>(), unionArray);
+        ArrayList<Integer> unionArray = union.findUnion(  new Integer[]{}, new Integer[]{0});
+        ArrayList<Integer> expected = new ArrayList<>();
+        expected.add(0);
+        assertEquals(expected, unionArray);
     }
 
     @Test
-    public void givenOneEmptyOneNull_returnEmptyList(){
+    public void given0ANd0_return0(){
         Union union = new Union();
-        ArrayList<Integer> unionArray = union.findUnion( new Integer[]{}, null);
-        assertEquals(new ArrayList<>(), unionArray);
+        ArrayList<Integer> unionArray = union.findUnion(  new Integer[]{0}, new Integer[]{0});
+        ArrayList<Integer> expected = new ArrayList<>();
+        expected.add(0);
+        assertEquals(expected, unionArray);
+    }
+
+    @Test
+    public void given0000ANd0000_return0(){
+        Union union = new Union();
+        ArrayList<Integer> unionArray = union.findUnion(  new Integer[]{0, 0, 0, 0}, new Integer[]{0, 0, 0, 0});
+        ArrayList<Integer> expected = new ArrayList<>();
+        expected.add(0);
+        assertEquals(expected, unionArray);
+    }
+
+    @Test
+    public void given012ANd0234_return0(){
+        Union union = new Union();
+        ArrayList<Integer> unionArray = union.findUnion(  new Integer[]{0, 1, 2}, new Integer[]{0, 2, 3, 4});
+        ArrayList<Integer> expected = new ArrayList<>();
+        expected.add(0);
+        expected.add(1);
+        expected.add(2);
+        expected.add(3);
+        expected.add(4);
+        assertEquals(expected, unionArray);
+    }
+
+    @Test
+    public void given012345ANd0234_return012345(){
+        Union union = new Union();
+        ArrayList<Integer> unionArray = union.findUnion(  new Integer[]{0, 1, 2, 3, 4, 5}, new Integer[]{0, 1, 2, 3, 4, 5});
+        ArrayList<Integer> expected = new ArrayList<>();
+        expected.add(0);
+        expected.add(1);
+        expected.add(2);
+        expected.add(3);
+        expected.add(4);
+        expected.add(5);
+        assertEquals(expected, unionArray);
     }
 }
