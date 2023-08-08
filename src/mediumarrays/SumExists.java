c4b4c49f2c4a8784d703594c82146aaa59108395
CHARAN TEJA <charan.teja@improve10x.com>
2023-08-08 15:02:45 +0530

    I did SumExists program by using  tdd

--- src/mediumarrays/SumExists.java
+++ src/mediumarrays/SumExists.java
@@ -2,6 +2,18 @@ package mediumarrays;
 
 public class SumExists {
     public String sumOfTwo(int[] books, int target) {
+        if (books == null || books.length <= 1){
+            return "No";
+        }
+
+        int n = books.length;
+        for (int i = 0; i < n; i++){
+            for (int j = i; j<n; j++){
+                if (books[i] + books[j] == target){
+                    return "Yes";
+                }
+            }
+        }
         return "No";
     }
 }
--- src/mediumarrays/SumExistsTest.java
+++ src/mediumarrays/SumExistsTest.java
@@ -28,4 +28,32 @@ public class SumExistsTest {
         String result = sumExists.sumOfTwo(new int[]{0}, 0);
         assertEquals("No", result);
     }
+
+    @Test
+    public void givenZeroOne_returnYes(){
+        SumExists sumExists = new SumExists();
+        String result = sumExists.sumOfTwo(new int[]{0, 1}, 1);
+        assertEquals("Yes", result);
+    }
+
+    @Test
+    public void givenOneOne_returnYes(){
+        SumExists sumExists = new SumExists();
+        String result = sumExists.sumOfTwo(new int[]{1, 1}, 2);
+        assertEquals("Yes", result);
+    }
+
+    @Test
+    public void given1123_returnYes(){
+        SumExists sumExists = new SumExists();
+        String result = sumExists.sumOfTwo(new int[]{1, 1, 2, 3}, 5);
+        assertEquals("Yes", result);
+    }
+
+    @Test
+    public void given_returnYes(){
+        SumExists sumExists = new SumExists();
+        String result = sumExists.sumOfTwo(new int[]{2, 6, 5, 8, 11}, 14);
+        assertEquals("Yes", result);
+    }
 }
