9c5e088405d59ad89044213803fb93861d5d7095
CHARAN TEJA <charan.teja@improve10x.com>
2023-08-08 10:53:48 +0530

    i did test for moving the zeros to the end.

--- src/movezerostoend/MoveZeros.java
+++ src/movezerostoend/MoveZeros.java
@@ -1,9 +1,27 @@
 package movezerostoend;
 
+import java.util.ArrayList;
+
 public class MoveZeros {
     public int[] moveZerosToTheEnd(int[] a, int n) {
         if (a == null || a.length == 0){
             return new int[]{};
+        }
+        ArrayList<Integer> temp = new ArrayList<>();
+        n = a.length;
+        for (int i = 0; i < n; i++){
+            if (a[i] != 0){
+                temp.add(a[i]);
+            }
+        }
+
+        int nz = temp.size();
+        for (int i = 0; i < nz; i++){
+            a[i] = temp.get(i);
+        }
+
+        for (int i = nz; i< n; i++){
+            a[i] = 0;
         }
         return a;
     }
--- src/movezerostoend/MoveZerosTest.java
+++ src/movezerostoend/MoveZerosTest.java
@@ -36,4 +36,32 @@ public class MoveZerosTest {
         int[] endArray = moveZeros.moveZerosToTheEnd(new int[]{1}, 1);
         assertArrayEquals(new int[]{1}, endArray);
     }
+
+    @Test
+    public void givenZeroOne_returnOneZero(){
+        MoveZeros moveZeros = new MoveZeros();
+        int[] endArray = moveZeros.moveZerosToTheEnd(new int[]{0, 1}, 2);
+        assertArrayEquals(new int[]{1, 0}, endArray);
+    }
+
+    @Test
+    public void givenZeroZero_returnZeroZero(){
+        MoveZeros moveZeros = new MoveZeros();
+        int[] endArray = moveZeros.moveZerosToTheEnd(new int[]{0, 0}, 2);
+        assertArrayEquals(new int[]{0, 0}, endArray);
+    }
+
+    @Test
+    public void givenZeroOneZero_returnOneZeroZero(){
+        MoveZeros moveZeros = new MoveZeros();
+        int[] endArray = moveZeros.moveZerosToTheEnd(new int[]{0, 1, 0}, 3);
+        assertArrayEquals(new int[]{1, 0, 0}, endArray);
+    }
+
+    @Test
+    public void given010101_return111000(){
+        MoveZeros moveZeros = new MoveZeros();
+        int[] endArray = moveZeros.moveZerosToTheEnd(new int[]{0, 1, 0, 1, 0, 1}, 6);
+        assertArrayEquals(new int[]{1, 1, 1, 0, 0, 0}, endArray);
+    }
 }
