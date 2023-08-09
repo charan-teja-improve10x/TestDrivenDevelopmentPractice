c0f4637d5a3f6812ee29525147511200df136030
CHARAN TEJA <charan.teja@improve10x.com>
2023-08-09 17:56:51 +0530

    I did some code refactoring in LeadersTest

--- src/mediumarrays/LeadersTest.java
+++ src/mediumarrays/LeadersTest.java
@@ -1,5 +1,6 @@
 package mediumarrays;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -8,55 +9,54 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LeadersTest {
 
+    private Leaders leaders;
+    private ArrayList<Integer> expected;
+
+    @BeforeEach
+    public void setup(){
+        leaders = new Leaders();
+        expected = new ArrayList<>();
+    }
+
     @Test
     public void noting(){}
 
     @Test
     public void givenNull_returnEmptyList(){
-        Leaders leaders = new Leaders();
         ArrayList<Integer> list = leaders.getLeaders(null, 0);
         assertEquals(new ArrayList<>(), list);
     }
 
     @Test
     public void givenEmpty_returnEmptyList(){
-        Leaders leaders = new Leaders();
         ArrayList<Integer> list = leaders.getLeaders(new int[]{}, 0);
         assertEquals(new ArrayList<>(), list);
     }
 
     @Test
     public void givenOneElement_returnOneList(){
-        Leaders leaders = new Leaders();
         ArrayList<Integer> list = leaders.getLeaders(new int[]{0}, 1);
-        ArrayList<Integer> expected = new ArrayList<>();
         expected.add(0);
         assertEquals(expected, list);
     }
 
     @Test
     public void givenZeroOne_returnOneList(){
-        Leaders leaders = new Leaders();
         ArrayList<Integer> list = leaders.getLeaders(new int[]{0, 1}, 2);
-        ArrayList<Integer> expected = new ArrayList<>();
         expected.add(1);
         assertEquals(expected, list);
     }
 
     @Test
     public void givenZeroOne2345_return3List(){
-        Leaders leaders = new Leaders();
         ArrayList<Integer> list = leaders.getLeaders(new int[]{0, 1, 2, 3}, 6);
-        ArrayList<Integer> expected = new ArrayList<>();
         expected.add(3);
         assertEquals(expected, list);
     }
 
     @Test
     public void given3210_return5List(){
-        Leaders leaders = new Leaders();
         ArrayList<Integer> list = leaders.getLeaders(new int[]{3, 2, 1, 0}, 4);
-        ArrayList<Integer> expected = new ArrayList<>();
         expected.add(3);
         expected.add(2);
         expected.add(1);
@@ -66,9 +66,7 @@ public class LeadersTest {
 
     @Test
     public void given13012_return5List(){
-        Leaders leaders = new Leaders();
         ArrayList<Integer> list = leaders.getLeaders(new int[]{1, 3, 0, 1, 2}, 5);
-        ArrayList<Integer> expected = new ArrayList<>();
         expected.add(3);
         expected.add(2);
         assertEquals(expected, list);
