1ceb1c9301de65ddd254996ecc02edd80b6e9a73
CHARAN TEJA <charan.teja@improve10x.com>
2023-08-09 17:25:31 +0530

    I did test cases for the LeadersTest

--- src/mediumarrays/Leaders.java
+++ src/mediumarrays/Leaders.java
@@ -1,9 +1,16 @@
 package mediumarrays;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Leaders {
     public ArrayList<Integer> getLeaders(int[] numbers, int n) {
-        return new ArrayList<>();
+        if (numbers == null|| numbers.length == 0){
+            return new ArrayList<>();
+        }else {
+            ArrayList<Integer> integers = new ArrayList<>();
+            integers.add(numbers[0]);
+            return integers;
+        }
     }
 }
--- src/mediumarrays/LeadersTest.java
+++ src/mediumarrays/LeadersTest.java
@@ -17,4 +17,20 @@ public class LeadersTest {
         ArrayList<Integer> list = leaders.getLeaders(null, 0);
         assertEquals(new ArrayList<>(), list);
     }
+
+    @Test
+    public void givenEmpty_returnEmptyList(){
+        Leaders leaders = new Leaders();
+        ArrayList<Integer> list = leaders.getLeaders(new int[]{}, 0);
+        assertEquals(new ArrayList<>(), list);
+    }
+
+    @Test
+    public void givenOneElement_returnOneList(){
+        Leaders leaders = new Leaders();
+        ArrayList<Integer> list = leaders.getLeaders(new int[]{0}, 1);
+        ArrayList<Integer> expected = new ArrayList<>();
+        expected.add(0);
+        assertEquals(expected, list);
+    }
 }
