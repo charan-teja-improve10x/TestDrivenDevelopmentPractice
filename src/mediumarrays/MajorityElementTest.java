314a096f08c10008bead2e17fd3853bc69dcf540
CHARAN TEJA <charan.teja@improve10x.com>
2023-08-09 15:25:15 +0530

    I did some code refactoring in MajorityElementTest class.

--- src/mediumarrays/MajorityElementTest.java
+++ src/mediumarrays/MajorityElementTest.java
@@ -1,73 +1,72 @@
 package mediumarrays;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MajorityElementTest {
 
+    private MajorityElement majorityElement;
+
+    @BeforeEach
+    public void setup(){
+        majorityElement = new MajorityElement();
+    }
+
     @Test
     public void nothing(){}
 
     @Test
     public void givenNull_returnMinusOne(){
-        MajorityElement majorityElement = new MajorityElement();
         int element = majorityElement.getMajorityElement(null);
         assertEquals(-1, element);
     }
 
     @Test
     public void givenEmpty_returnMinusOne(){
-        MajorityElement majorityElement = new MajorityElement();
         int element = majorityElement.getMajorityElement(new int[]{});
         assertEquals(-1, element);
     }
 
     @Test
     public void givenZero_returnZero(){
-        MajorityElement majorityElement = new MajorityElement();
         int element = majorityElement.getMajorityElement(new int[]{0});
         assertEquals(0, element);
     }
 
     @Test
     public void givenOne_returnOne(){
-        MajorityElement majorityElement = new MajorityElement();
         int element = majorityElement.getMajorityElement(new int[]{1});
         assertEquals(1, element);
     }
 
     @Test
     public void givenZeroOne_returnMinusOne(){
-        MajorityElement majorityElement = new MajorityElement();
         int element = majorityElement.getMajorityElement(new int[]{0, 1});
         assertEquals(-1, element);
     }
 
     @Test
     public void givenZeroOneOne_returnOne(){
-        MajorityElement majorityElement = new MajorityElement();
         int element = majorityElement.getMajorityElement(new int[]{0, 1, 1});
         assertEquals(1, element);
     }
 
     @Test
     public void givenZeroOneOneZero_returnMinusOne(){
-        MajorityElement majorityElement = new MajorityElement();
         int element = majorityElement.getMajorityElement(new int[]{0, 1, 1, 0});
         assertEquals(-1, element);
     }
 
     @Test
     public void given44333_return3(){
-        MajorityElement majorityElement = new MajorityElement();
         int element = majorityElement.getMajorityElement(new int[]{4, 4, 3, 3, 3});
         assertEquals(3, element);
     }
 
     @Test
     public void given0011122_return3(){
-        MajorityElement majorityElement = new MajorityElement();
         int element = majorityElement.getMajorityElement(new int[]{0, 0, 1, 1, 1, 2, 2});
         assertEquals(-1, element);
     }
