6b6af4923f8abee2d5cd3ad20f2dc82a6065075c
CHARAN TEJA <charan.teja@improve10x.com>
2023-08-10 00:12:46 +0530

    I did some refactoring in logic

--- src/mediumarrays/StockBuyAndSell.java
+++ src/mediumarrays/StockBuyAndSell.java
@@ -4,12 +4,12 @@ public class StockBuyAndSell {
     public int maximumProfit(int[] prizes) {
         if (prizes == null || prizes.length == 0){
             return -1;
-        } else if (prizes.length == 1 || prizes.length == 2) {
+        } else if (prizes.length == 1){
             return 0;
         } else {
             int maxProf = 0;
             for (int i = 0; i < prizes.length; i++){
-                for (int j = i + 1; j < prizes.length; j++){
+                for (int j = i ; j < prizes.length; j++){
                     if (prizes[j] > prizes[i]){
                         maxProf = Math.max(prizes[j] - prizes[i], maxProf);
                     }
--- src/mediumarrays/StockBuyAndSellTest.java
+++ src/mediumarrays/StockBuyAndSellTest.java
@@ -1,5 +1,7 @@
 package mediumarrays;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -33,34 +35,36 @@ public class StockBuyAndSellTest {
     public void givenOne2_return0(){
         StockBuyAndSell stockBuyAndSell = new StockBuyAndSell();
         int max = stockBuyAndSell.maximumProfit(new int[]{1, 2});
-        assertEquals(0, max);
+        assertEquals(1, max);
     }
 
     @Test
     public void givenOne23_return1(){
         StockBuyAndSell stockBuyAndSell = new StockBuyAndSell();
-        int max = stockBuyAndSell.maximumProfit(new int[]{3, 2, 3});
+        int max = stockBuyAndSell.maximumProfit(new int[]{2, 2, 3});
         assertEquals(1, max);
     }
+
 
     @Test
     public void givenOne234_return2(){
         StockBuyAndSell stockBuyAndSell = new StockBuyAndSell();
-        int max = stockBuyAndSell.maximumProfit(new int[]{7, 2, 3, 4});
+        int max = stockBuyAndSell.maximumProfit(new int[]{2, 2, 3, 4});
         assertEquals(2, max);
     }
 
     @Test
     public void given234_return2(){
         StockBuyAndSell stockBuyAndSell = new StockBuyAndSell();
-        int max = stockBuyAndSell.maximumProfit(new int[]{7, 2, 3, 4});
-        assertEquals(2, max);
+        int max = stockBuyAndSell.maximumProfit(new int[]{1, 2, 3, 4});
+        assertEquals(3, max);
     }
+
 
     @Test
     public void given42347_return1(){
         StockBuyAndSell stockBuyAndSell = new StockBuyAndSell();
-        int max = stockBuyAndSell.maximumProfit(new int[]{4, 2, 3, 4, 7, 1});
+        int max = stockBuyAndSell.maximumProfit(new int[]{7, 2, 3, 4, 7, 1});
         assertEquals(5, max);
     }
 }
