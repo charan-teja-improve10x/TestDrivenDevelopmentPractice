da4ee107319acebbd6e095728b3a8132d2527cc7
CHARAN TEJA <charan.teja@improve10x.com>
2023-08-09 15:59:50 +0530

    I did some code refactoring in MajorityElement

--- src/mediumarrays/MajorityElement.java
+++ src/mediumarrays/MajorityElement.java
@@ -1,23 +1,24 @@
 package mediumarrays;
 
+import java.util.Arrays;
+
 public class MajorityElement {
-    public int getMajorityElement(int[]elements) {
-        if (elements == null || elements.length == 0){
-            return -1;
-        }else {
+
+    public static int getMajorityElement(int[]elements) {
+        if (elements != null && elements.length != 0) {
             int n = elements.length;
-            for ( int i = 0; i< n; i++){
+            for (int i = 0; i < n; i++) {// 2, 2, 1, 1, 1, 2, 2
                 int count = 0;
-                for (int j = 0; j< n; j++){
-                    if (elements[i] == elements[j]){
+                for (int j = 0; j < n; j++) {
+                    if (elements[i] == elements[j]) {//2, 2, 1, 1, 1, 2, 2
                         count++;
                     }
                 }
-                if (count > n/2){
+                if (count > n / 2) {
                     return elements[i];
                 }
             }
-            return -1;
         }
+        return -1;
     }
 }
