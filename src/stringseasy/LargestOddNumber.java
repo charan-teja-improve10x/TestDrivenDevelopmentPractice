0d9cf3841f3c15593bff7592f7df59368a418124
CHARAN TEJA <charan.teja@improve10x.com>
2023-08-10 14:41:46 +0530

    I did some code refactoring in LargestOddNumber logic

--- src/stringseasy/LargestOddNumber.java
+++ src/stringseasy/LargestOddNumber.java
@@ -2,16 +2,14 @@ package stringseasy;
 
 public class LargestOddNumber {
     public String findLargestOddNUmber(String num) {
-        if (num == null || num.equals("")) {
-            return "";
-        } else {
+        if (num != null && !num.equals("")) {
             String number = num.trim();
             for (int i = number.length() - 1; i >= 0; i--) {
                 if (number.charAt(i) % 2 != 0) {
                     return number.substring(0, i + 1);
                 }
             }
-            return "";
         }
+        return "";
     }
 }
