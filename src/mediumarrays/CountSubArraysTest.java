7557735f8b4fc02a88a8ca90c323e0a9f24b5e6c
CHARAN TEJA <charan.teja@improve10x.com>
2023-08-10 11:00:42 +0530

    I did some code  refactoring in CountSubArraysTest class

--- src/mediumarrays/CountSubArraysTest.java
+++ src/mediumarrays/CountSubArraysTest.java
@@ -1,5 +1,6 @@
 package mediumarrays;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,51 +10,51 @@ public class CountSubArraysTest {
     @Test
     public void nothing(){}
 
+    private CountSubArrays countSubArrays;
+
+    @BeforeEach
+    public void setup(){
+        countSubArrays = new CountSubArrays();
+    }
+
     @Test
     public void givenNull_returnMinusOne(){
-        CountSubArrays countSubArrays = new CountSubArrays();
         int count = countSubArrays.findTheSubArraysWithGivenSum(null, 0);
         assertEquals(-1, count);
     }
 
     @Test
     public void givenEmpty_returnMinusOne(){
-        CountSubArrays countSubArrays = new CountSubArrays();
         int count = countSubArrays.findTheSubArraysWithGivenSum(new int[]{}, 0);
         assertEquals(-1, count);
     }
 
     @Test
     public void givenOneAndOne_returnOne(){
-        CountSubArrays countSubArrays = new CountSubArrays();
         int count = countSubArrays.findTheSubArraysWithGivenSum(new int[]{1}, 1);
         assertEquals(1, count);
     }
 
     @Test
     public void given012AndThree_returnTwo(){
-        CountSubArrays countSubArrays = new CountSubArrays();
         int count = countSubArrays.findTheSubArraysWithGivenSum(new int[]{0, 1, 2}, 3);
         assertEquals(2, count);
     }
 
     @Test
     public void given0123AndThree_returnTwo(){
-        CountSubArrays countSubArrays = new CountSubArrays();
         int count = countSubArrays.findTheSubArraysWithGivenSum(new int[]{1, 2, 3}, 3);
         assertEquals(2, count);
     }
 
     @Test
     public void given3124_return2(){
-        CountSubArrays countSubArrays = new CountSubArrays();
         int count = countSubArrays.findTheSubArraysWithGivenSum(new int[]{3, 1, 2, 4}, 6);
         assertEquals(2, count);
     }
 
     @Test
     public void given12_return1(){
-        CountSubArrays countSubArrays = new CountSubArrays();
         int count = countSubArrays.findTheSubArraysWithGivenSum(new int[]{1, 2}, 3);
         assertEquals(1, count);
     }
