4d993e29082fbe25e99404ac5c1e5da697b4c6e5
CHARAN TEJA <charan.teja@improve10x.com>
2023-08-10 15:37:10 +0530

    I wrote logic for getTheLorgestSubArrayByGivenSum

--- src/mediumarrays/easy/LongestSubArray.java
+++ src/mediumarrays/easy/LongestSubArray.java
@@ -6,7 +6,20 @@ public class LongestSubArray {
             return 0;
         } else if (numbers.length == 1 && numbers[0] == sum) {
             return 1;
+        }else {
+            int n = numbers.length;
+            int len = 0;
+            for (int i = 0; i < n; i++){
+                int s= 0;
+                for (int j = i; j< n; j++){
+                    s+= numbers[j];
+
+                    if (s == sum){
+                        len = Math.max(len, j - i + 1);
+                    }
+                }
+            }
+            return len;
         }
-        return 0;
     }
 }
--- src/mediumarrays/easy/LongestSubArrayTest.java
+++ src/mediumarrays/easy/LongestSubArrayTest.java
@@ -1,46 +1,60 @@
 package mediumarrays.easy;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LongestSubArrayTest {
 
+    private LongestSubArray longestSubArray;
+
+    @BeforeEach
+    public void setup(){
+        longestSubArray = new LongestSubArray();
+    }
     @Test
     public void nothing(){}
 
     @Test
     public void givenNull_returnZero(){
-        LongestSubArray longestSubArray = new LongestSubArray();
         int length = longestSubArray.findTheLongestSubArrayWithGivenSum(null, 0);
         assertEquals(0, length);
     }
 
     @Test
     public void givenEmpty_returnZero(){
-        LongestSubArray longestSubArray = new LongestSubArray();
         int length = longestSubArray.findTheLongestSubArrayWithGivenSum(new int[]{}, 0);
         assertEquals(0, length);
     }
 
     @Test
     public void given0_returnOne(){
-        LongestSubArray longestSubArray = new LongestSubArray();
         int length = longestSubArray.findTheLongestSubArrayWithGivenSum(new int[]{0}, 0);
         assertEquals(1, length);
     }
 
     @Test
     public void given1_returnOne(){
-        LongestSubArray longestSubArray = new LongestSubArray();
         int length = longestSubArray.findTheLongestSubArrayWithGivenSum(new int[]{1}, 1);
         assertEquals(1, length);
     }
 
     @Test
     public void given5_returnOne(){
-        LongestSubArray longestSubArray = new LongestSubArray();
         int length = longestSubArray.findTheLongestSubArrayWithGivenSum(new int[]{5}, 5);
         assertEquals(1, length);
+    }
+
+    @Test
+    public void given01And5_returnZero(){
+        int length = longestSubArray.findTheLongestSubArrayWithGivenSum(new int[]{0, 1}, 5);
+        assertEquals(0, length);
+    }
+
+    @Test
+    public void given01And1_return2(){
+        int length = longestSubArray.findTheLongestSubArrayWithGivenSum(new int[]{0, 1}, 1);
+        assertEquals(2, length);
     }
 }
