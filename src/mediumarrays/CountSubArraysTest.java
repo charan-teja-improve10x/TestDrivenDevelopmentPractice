1bd708580545db8c8c315579dc8637c87510e7f3
CHARAN TEJA <charan.teja@improve10x.com>
2023-08-10 10:44:41 +0530

    I have completed Counting The subArrays with given number

--- src/mediumarrays/CountSubArraysTest.java
+++ src/mediumarrays/CountSubArraysTest.java
@@ -31,16 +31,30 @@ public class CountSubArraysTest {
     }
 
     @Test
-    public void given012AndThree_returnZero(){
+    public void given012AndThree_returnTwo(){
         CountSubArrays countSubArrays = new CountSubArrays();
         int count = countSubArrays.findTheSubArraysWithGivenSum(new int[]{0, 1, 2}, 3);
         assertEquals(2, count);
     }
 
     @Test
-    public void given0123AndThree_returnZero(){
+    public void given0123AndThree_returnTwo(){
         CountSubArrays countSubArrays = new CountSubArrays();
         int count = countSubArrays.findTheSubArraysWithGivenSum(new int[]{1, 2, 3}, 3);
         assertEquals(2, count);
+    }
+
+    @Test
+    public void given3124_return2(){
+        CountSubArrays countSubArrays = new CountSubArrays();
+        int count = countSubArrays.findTheSubArraysWithGivenSum(new int[]{3, 1, 2, 4}, 6);
+        assertEquals(2, count);
+    }
+
+    @Test
+    public void given12_return1(){
+        CountSubArrays countSubArrays = new CountSubArrays();
+        int count = countSubArrays.findTheSubArraysWithGivenSum(new int[]{1, 2}, 3);
+        assertEquals(1, count);
     }
 }
