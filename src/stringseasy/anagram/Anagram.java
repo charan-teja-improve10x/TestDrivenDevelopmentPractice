ef8700749eaa091caaebd36e5fc2be4dbc5460b0
CHARAN TEJA <charan.teja@improve10x.com>
2023-08-10 16:51:50 +0530

    I did logic for testing the Anagram

--- src/stringseasy/anagram/Anagram.java
+++ src/stringseasy/anagram/Anagram.java
@@ -2,6 +2,16 @@ package stringseasy.anagram;
 
 public class Anagram {
     public boolean isAnagram(String str1, String str2) {
-        return false;
+        if (str1 == null || str2 == null || str1.equals("") || str2.equals("")){
+            return false;
+        } else if (str1.length() != str2.length()) {
+            return false;
+        }
+        for (int i = 0; i< str1.length(); i++){
+            if (str1.charAt(i) != str2.charAt(i)){
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/stringseasy/anagram/AnagramTest.java
+++ src/stringseasy/anagram/AnagramTest.java
@@ -1,25 +1,55 @@
 package stringseasy.anagram;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AnagramTest {
 
+    private Anagram anagram;
+
+    @BeforeEach
+    public void setup(){
+        anagram = new Anagram();
+    }
+
     @Test
     public void nothing(){}
 
     @Test
     public void givenNull_returnFalse(){
-        Anagram anagram = new Anagram();
         boolean result = anagram.isAnagram(null, null);
         assertEquals(false, result);
     }
 
     @Test
     public void givenEmpty_returnFalse(){
-        Anagram anagram = new Anagram();
         boolean result = anagram.isAnagram("", "");
         assertEquals(false, result);
+    }
+
+    @Test
+    public void givenNullAndEmpty_returnFalse(){
+        boolean result = anagram.isAnagram(null, "");
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void givenEmptyAndNull_returnFalse(){
+        boolean result = anagram.isAnagram("", null);
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void givenAAndB_returnFalse(){
+        boolean result = anagram.isAnagram("A", "B");
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void givenAAndA_returnTrue(){
+        boolean result = anagram.isAnagram("A", "A");
+        assertEquals(true, result);
     }
 }
