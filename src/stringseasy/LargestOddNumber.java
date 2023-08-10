17cd14b46555e6f985a17332bd4f5de7e57468eb
CHARAN TEJA <charan.teja@improve10x.com>
2023-08-10 12:08:30 +0530

    I wrote logic for finding the largest Odd Number

--- src/stringseasy/LargestOddNumber.java
+++ src/stringseasy/LargestOddNumber.java
@@ -2,6 +2,15 @@ package stringseasy;
 
 public class LargestOddNumber {
     public String findLargestOddNUmber(String num) {
-        return "";
+        if (num == null || num.length() == 0) {
+            return "";
+        } else {
+            for (int i = num.length() - 1; i >= 0; i--) {
+                if (num.charAt(i) % 2 != 0) {
+                    return num.substring(0, i + 1);
+                }
+            }
+            return "";
+        }
     }
 }
--- src/stringseasy/LargestOddNumberTest.java
+++ src/stringseasy/LargestOddNumberTest.java
@@ -12,9 +12,38 @@ public class LargestOddNumberTest {
     }
 
     @Test
-    public void givenNull_returnNull(){
+    public void givenNull_returnEmpty(){
       LargestOddNumber largestOddNumber = new LargestOddNumber();
       String ans = largestOddNumber.findLargestOddNUmber(null);
       assertEquals("", ans);
     }
+
+    @Test
+    public void givenEmpty_returnEmpty(){
+        LargestOddNumber largestOddNumber = new LargestOddNumber();
+        String ans = largestOddNumber.findLargestOddNUmber("");
+        assertEquals("", ans);
+    }
+
+    @Test
+    public void given0_returnEmpty(){
+        LargestOddNumber largestOddNumber = new LargestOddNumber();
+        String ans = largestOddNumber.findLargestOddNUmber("");
+        assertEquals("", ans);
+    }
+
+    @Test
+    public void given1_return1(){
+        LargestOddNumber largestOddNumber = new LargestOddNumber();
+        String ans = largestOddNumber.findLargestOddNUmber("1");
+        assertEquals("1", ans);
+    }
+
+    @Test
+    public void given52_return5(){
+        LargestOddNumber largestOddNumber = new LargestOddNumber();
+        String ans = largestOddNumber.findLargestOddNUmber("52");
+        assertEquals("5", ans);
+    }
+
 }
