11b952693332217b99d7d03c4285d1f02091b370
CHARAN TEJA <charan.teja@improve10x.com>
2023-08-11 11:01:08 +0530

    I did some code refactoring In WordReverseTest class

--- src/stringsmedium/WordReverseTest.java
+++ src/stringsmedium/WordReverseTest.java
@@ -1,24 +1,30 @@
 package stringsmedium;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WordReverseTest {
 
+    private WordReverse wordReverse;
+
+    @BeforeEach
+    public void setup(){
+        wordReverse = new WordReverse();
+    }
+
     @Test
     public void nothing(){}
 
     @Test
     public void givenNull_returnEmpty(){
-        WordReverse wordReverse = new WordReverse();
         String reverse = wordReverse.reverseWords(null);
         assertEquals("", reverse);
     }
 
     @Test
     public void givenEmpty_returnEmpty(){
-        WordReverse wordReverse = new WordReverse();
         String reverse = wordReverse.reverseWords("");
         assertEquals("", reverse);
     }
