c2a19093e784bce2d5ec780aeefafb7b4e8c3696
CHARAN TEJA <charan.teja@improve10x.com>
2023-08-11 10:59:34 +0530

    I have created WordReverse class And did two test cases

---
+++ src/stringsmedium/WordReverse.java
@@ -0,0 +1,7 @@
+package stringsmedium;
+
+public class WordReverse {
+    public String reverseWords(String word) {
+        return "";
+    }
+}
--- src/stringsmedium/WordReverseTest.java
+++ src/stringsmedium/WordReverseTest.java
@@ -2,8 +2,24 @@ package stringsmedium;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class WordReverseTest {
 
     @Test
     public void nothing(){}
+
+    @Test
+    public void givenNull_returnEmpty(){
+        WordReverse wordReverse = new WordReverse();
+        String reverse = wordReverse.reverseWords(null);
+        assertEquals("", reverse);
+    }
+
+    @Test
+    public void givenEmpty_returnEmpty(){
+        WordReverse wordReverse = new WordReverse();
+        String reverse = wordReverse.reverseWords("");
+        assertEquals("", reverse);
+    }
 }
