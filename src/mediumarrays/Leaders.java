56e89953a2e0d05ec9229cd4682231665645116e
CHARAN TEJA <charan.teja@improve10x.com>
2023-08-11 11:55:42 +0530

    I did some code refactoring in Leaders in java class.

--- src/mediumarrays/Leaders.java
+++ src/mediumarrays/Leaders.java
@@ -1,17 +1,12 @@
 package mediumarrays;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Leaders {
     public ArrayList<Integer> getLeaders(int[] numbers, int n) {
         if (numbers == null|| numbers.length == 0){
             return new ArrayList<>();
-        }else if (numbers.length == 1){
-            ArrayList<Integer> integers = new ArrayList<>();
-            integers.add(numbers[0]);
-            return integers;
-        }else{
+        } else{
             ArrayList<Integer> integers = new ArrayList<>();
              n = numbers.length;
             for (int i = 0; i < n; i++){
