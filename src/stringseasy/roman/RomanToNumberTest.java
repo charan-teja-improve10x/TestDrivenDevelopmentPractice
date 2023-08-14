02ce988ffa606a6dde0b964c51058da55fcadb29
CHARAN TEJA <charan.teja@improve10x.com>
2023-08-14 15:22:59 +0530

    I have completed RomanToNumber into Numbers

--- src/stringseasy/roman/RomanToNumberTest.java
+++ src/stringseasy/roman/RomanToNumberTest.java
@@ -1,53 +1,65 @@
 package stringseasy.roman;
 
 import org.junit.jupiter.api.Test;
-import templerun.Coin;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RomanToNumberTest {
 
+    private static RomanToNumber romanToNumber = new RomanToNumber();
+
     @Test
     public void nothing(){}
 
     @Test
     public void givenNull_returnZero(){
-        RomanToNumber romanToNumber = new RomanToNumber();
         int number = romanToNumber.romanToInt(null);
         assertEquals(0, number);
     }
 
     @Test
     public void givenEmpty_returnZero(){
-        RomanToNumber romanToNumber = new RomanToNumber();
         int number = romanToNumber.romanToInt("");
         assertEquals(0, number);
     }
 
     @Test
     public void givenI_returnOne(){
-        RomanToNumber romanToNumber = new RomanToNumber();
         int number = romanToNumber.romanToInt("I");
         assertEquals(1, number);
     }
 
     @Test
     public void givenII_return2(){
-        RomanToNumber romanToNumber = new RomanToNumber();
         int number = romanToNumber.romanToInt("II");
         assertEquals(2, number);
     }
 
     @Test
     public void givenOtherCharacter_throwInvalidInputException(){
-        RomanToNumber romanToNumber = new RomanToNumber();
         assertThrows(RomanToNumber.InvalidInputException.class, ()-> romanToNumber.romanToInt("s"), "Enter Roman Values Only...!");
     }
 
     @Test
     public void givenSmallI_throwInvalidInputException(){
-        RomanToNumber romanToNumber = new RomanToNumber();
         assertThrows(RomanToNumber.InvalidInputException.class, ()-> romanToNumber.romanToInt("i"), "Enter Roman Values Only...!");
+    }
+
+    @Test
+    public void givenXIII_return13(){
+        int number = romanToNumber.romanToInt("XIII");
+        assertEquals(13, number);
+    }
+    @Test
+    public void givenLVIII_return2(){
+        int number = romanToNumber.romanToInt("LVIII");
+        assertEquals(58, number);
+    }
+
+    @Test
+    public void givenMCMXCIV_return1994(){
+        int number = romanToNumber.romanToInt("MCMXCIV");
+        assertEquals(1994, number);
     }
 }
